7ce5352cd99d9bdb167a236cdbecb125688a35b1
Jaisen Mathai <jaisen@jmathai.com>
2011-07-19 14:42:33 -0700

    Initialization now creates databases for user and social content.

--- src/libraries/adapters/DatabaseSimpleDb.php
+++ src/libraries/adapters/DatabaseSimpleDb.php
@@ -1,28 +1,30 @@
 <?php
 class DatabaseSimpleDb implements DatabaseInterface
 {
-  private $domain;
+  private $domainPhoto, $domainSocial, $domainUser;
   public function __construct($opts)
   {
     $this->db = new AmazonSDB($opts->awsKey, $opts->awsSecret);
-    $this->domain = getConfig()->get('aws')->simpleDbDomain;
+    $this->domainPhoto = getConfig()->get('aws')->simpleDbDomain;
+    $this->domainSocial = getConfig()->get('aws')->simpleDbDomain.'Social';
+    $this->domainUser = getConfig()->get('aws')->simpleDbDomain.'User';
   }
 
   public function addAttribute($id, $keyValuePairs, $replace = true)
   {
-    $res = $this->db->put_attributes($this->domain, $id, $keyValuePairs, $replace);
+    $res = $this->db->put_attributes($this->domainPhoto, $id, $keyValuePairs, $replace);
     return $res->isOK();
   }
 
   public function deletePhoto($id)
   {
-    $res = $this->db->delete_attributes($this->domain, $id);
+    $res = $this->db->delete_attributes($this->domainPhoto, $id);
     return $res->isOK();
   }
 
   public function getPhoto($id)
   {
-    $res = $this->db->select("select * from `{$this->domain}` where itemName()='{$id}'", array('ConsistentRead' => 'true'));
+    $res = $this->db->select("select * from `{$this->domainPhoto}` where itemName()='{$id}'", array('ConsistentRead' => 'true'));
     if(isset($res->body->SelectResult->Item))
       return self::normalizePhoto($res->body->SelectResult->Item);
     else
@@ -69,7 +71,7 @@ class DatabaseSimpleDb implements DatabaseInterface
       $thisLimit = min($iterator, $offset);
       do
       {
-        $res = $this->db->select("select * from `{$this->domain}` {$where} {$sortBy} limit {$iterator}", $params);
+        $res = $this->db->select("select * from `{$this->domainPhoto}` {$where} {$sortBy} limit {$iterator}", $params);
         if(!$res->body->SelectResult->NextToken)
           break;
 
@@ -85,10 +87,10 @@ class DatabaseSimpleDb implements DatabaseInterface
 
 
     $queue = new CFBatchRequest();
-    $this->db->batch($queue)->select($sql = "select * from `{$this->domain}` {$where} {$sortBy} limit {$limit}", $params);
+    $this->db->batch($queue)->select($sql = "select * from `{$this->domainPhoto}` {$where} {$sortBy} limit {$limit}", $params);
     if(isset($params['NextToken']))
       unset($params['NextToken']);
-    $this->db->batch($queue)->select("select count(*) from `{$this->domain}` {$where}", $params);
+    $this->db->batch($queue)->select("select count(*) from `{$this->domainPhoto}` {$where}", $params);
     $responses = $this->db->batch($queue)->send();
 
     if(!$responses->areOK())
@@ -105,24 +107,30 @@ class DatabaseSimpleDb implements DatabaseInterface
   public function postPhoto($id, $params)
   {
     $params = self::preparePhoto($id, $params);
-    $res = $this->db->put_attributes($this->domain, $id, $params, true);
+    $res = $this->db->put_attributes($this->domainPhoto, $id, $params, true);
     return $res->isOK();
   }
 
   public function putPhoto($id, $params)
   {
-    $res = $this->db->put_attributes($this->domain, $id, $params);
+    $res = $this->db->put_attributes($this->domainPhoto, $id, $params);
     return $res->isOK();
   }
 
   public function initialize()
   {
-    $domains = $this->db->get_domain_list("/^{$this->domain}$/");
-    if(count($domains) == 1)
+    $domains = $this->db->get_domain_list("/^{$this->domainPhoto}(User|Social)?$/");
+    if(count($domains) == 3)
       return true;
+    elseif(count($domains) != 0)
+      return false;
 
-    $res = $this->db->create_domain($this->domain);
-    return $res->isOK();
+    $queue = new CFBatchRequest();
+    $this->db->batch($queue)->create_domain($this->domainPhoto);
+    $this->db->batch($queue)->create_domain($this->domainSocial);
+    $this->db->batch($queue)->create_domain($this->domainUser);
+    $responses = $this->db->batch($queue)->send();
+    return $responses->areOK();
   }
 
   private function normalizePhoto($raw)
