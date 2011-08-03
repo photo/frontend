17a0bd0772319c338155fcb90344bb6c04d6030e
Jaisen Mathai <jaisen@jmathai.com>
2011-08-02 21:01:42 -0700

    Reverting setup initialization to be inline in index.php.

--- src/html/index.php
+++ src/html/index.php
@@ -35,6 +35,16 @@ if(file_exists($configFile))
 elseif(!file_exists($configFile))
 {
   // setup and enable routes for setup
-  require getConfig()->get('paths')->libraries . '/setupInit.php';
+  $baseDir = dirname(dirname(__FILE__));
+  $paths = new stdClass;
+  $paths->libraries = "{$baseDir}/libraries";
+  $paths->controllers = "{$baseDir}/libraries/controllers";
+  $paths->external = "{$baseDir}/libraries/external";
+  $paths->adapters = "{$baseDir}/libraries/adapters";
+  $paths->models = "{$baseDir}/libraries/models";
+  getConfig()->set('paths', $paths);
+  require getConfig()->get('paths')->libraries . '/dependencies.php';
+  require getConfig()->get('paths')->libraries . '/routes-setup.php';
+  require getConfig()->get('paths')->controllers . '/SetupController.php';
   getRoute()->run('/setup');
 }
