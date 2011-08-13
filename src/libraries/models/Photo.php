ae2c29a507c79ab1ff8378daeba08e8efe3e8315
Hubert Figuiere <hub@figuiere.net>
2011-08-13 13:45:18 -0700

    Add support for the basic Exif (gh-52)

--- src/libraries/models/Photo.php
+++ src/libraries/models/Photo.php
@@ -290,6 +290,10 @@ class Photo
           'size' => intval(filesize($localFile)/1024),
           'exifCameraMake' => @$exif['cameraMake'],
           'exifCameraModel' => @$exif['cameraModel'],
+          'exifFNumber' => @$exif['FNumber'],
+          'exifExposureTime' => @$exif['exposureTime'],
+          'exifISOSpeed' => @$exif['ISO'],
+          'exifFocalLength' => @$exif['focalLength'],
           'width' => @$exif['width'],
           'height' => @$exif['height'],
           'dateTaken' => $dateTaken,
@@ -376,35 +380,35 @@ class Photo
     return (substr(sha1(implode('.', $args)), 0, 5) == $hash);
   }
 
-
-  /*** GPS Utils 
-   * from http://stackoverflow.com/questions/2526304/php-extract-gps-exif-data 
-   **/
-  private static function getGps($exifCoord, $hemi) 
+  private static function frac2Num($frac) 
   {
-    $degrees = count($exifCoord) > 0 ? self::gps2Num($exifCoord[0]) : 0;
-    $minutes = count($exifCoord) > 1 ? self::gps2Num($exifCoord[1]) : 0;
-    $seconds = count($exifCoord) > 2 ? self::gps2Num($exifCoord[2]) : 0;
-
-    $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
-
-    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
-  }
-
-  private static function gps2Num($coordPart) 
-  {
-    $parts = explode('/', $coordPart);
+    $parts = explode('/', $frac);
 
     if (count($parts) <= 0)
         return 0;
 
     if (count($parts) == 1)
         return $parts[0];
+    // DIV/0
+    if($parts[1] == 0)
+        return 0;
 
     return floatval($parts[0]) / floatval($parts[1]);
   }
 
+  /*** GPS Utils 
+   * from http://stackoverflow.com/questions/2526304/php-extract-gps-exif-data 
+   **/
+  private static function getGps($exifCoord, $hemi) 
+  {
+    $degrees = count($exifCoord) > 0 ? self::frac2Num($exifCoord[0]) : 0;
+    $minutes = count($exifCoord) > 1 ? self::frac2Num($exifCoord[1]) : 0;
+    $seconds = count($exifCoord) > 2 ? self::frac2Num($exifCoord[2]) : 0;
 
+    $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+
+    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+  }
 
   /**
     * Reads exif data from a photo.
@@ -417,7 +421,6 @@ class Photo
     $exif = @exif_read_data($photo);
     if(!$exif)
       return null;
-
     $size = getimagesize($photo);
     $dateTaken = $exif['FileDateTime'];
     if(array_key_exists('DateTime', $exif))
@@ -430,7 +433,9 @@ class Photo
 
     $exif_array = array('dateTaken' => $dateTaken, 'width' => $size[0], 
       'height' => $size[1], 'cameraModel' => @$exif['Model'], 
-      'cameraMake' => @$exif['Make']);
+      'cameraMake' => @$exif['Make'],
+      'ISO' => @$exif['ISOSpeedRatings'],
+      'exposureTime' => @$exif['ExposureTime']);
 
     if(isset($exif['GPSLongitude'])) {
       $exif_array['longitude'] = self::getGps($exif['GPSLongitude'], $exif['GPSLongitudeRef']);
@@ -439,6 +444,9 @@ class Photo
     if(isset($exif['GPSLatitude'])) {
       $exif_array['latitude'] = self::getGps($exif['GPSLatitude'], $exif['GPSLatitudeRef']);
     }
+
+    $exif_array['FNumber'] = self::frac2Num(@$exif['FNumber']);
+    $exif_array['focalLength'] = self::frac2Num(@$exif['FocalLength']);
 
     return $exif_array;
   }
