8a3688a8608c926aa5a2d58008bd818a01615484
Jaisen Mathai <jaisen@jmathai.com>
2011-08-14 20:10:30 -0700

    Adding initial theme directories and files. gh-32

---
+++ src/html/assets/themes/default/templates/photos.php
@@ -0,0 +1,32 @@
+        <div class="pagination">
+            <p class="label"><span class="audible">Pagination:</span>Page 3 of 5</p>
+            <ul role="navigation">
+                <li><a href="/photos/page-1"><span class="audible">Page</span>1</a></li>
+                <li><a href="/photos/page-2"><span class="audible">Page</span>2</a></li>
+
+                <li class="on"><p><span class="audible">You're currently reading page </span>3</p></li>
+                <li><a href="/photos/page-4"><span class="audible">Page</span>4</a></li>
+                <li><a href="/photos/page-5"><span class="audible">Page</span>5</a></li>
+            </ul>
+        </div>
+
+        <ul class="photo-grid grid-200">
+
+            <li class="grid-item">
+                <a href="/photo/4t"><img class="photo" src="http://opmecurrent.s3.amazonaws.com/custom/201108/1312348300-IMAG0015_200x200xCR.jpg" alt="Tavin riding the horsey at Murphy Park in Sunnyvale"></a>
+                <ul class="meta">
+                    <li class="age">Taken 3 days ago</li>
+                    <li class="permission private"><span class="audible">This photo is marked as </span>private</li>
+                    <li class="hastags"><img src="http://current.openphoto.me/assets/img/default/icon-tags.png" alt="" title="This photo has tags"></li>
+                    <li class="haslocationinfo"><img src="http://current.openphoto.me/assets/img/default/icon-globe.png" alt="" title="This photo has location information"></li>
+
+                </ul>
+<!--            <ul class="actions">
+
+                <li><a href="#"><img src="/assets/img/default/icon-add.png"></a></li>
+                <li><a href="#"><img src="/assets/img/default/icon-star.png"></a></li>
+            
+            </ul>-->
+            <!--(<a href="/photo/4t/delete" class="photo-delete">delete</a>) -->
+            <!--sunnyvale,tavin-->
+            </li>
