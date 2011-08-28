110923de003e0276549e59e600ddce51a12c5824
Jaisen Mathai <jaisen@jmathai.com>
2011-08-27 17:09:16 -0700

    Initial working version of a default theme using the theming framework. Gh-32

--- src/libraries/external/epi/EpiConfig.php
+++ src/libraries/external/epi/EpiConfig.php
@@ -13,7 +13,10 @@ class EpiConfig
     $args = func_get_args();
     foreach($args as $file)
     {
-      $file = Epi::getPath('config') . "/{$file}";
+      // Prepend config directory if the path doesn't start with . or /
+      if($file[0] != '.' && $file[0] != '/')
+        $file = Epi::getPath('config') . "/{$file}";
+
       if(!file_exists($file))
       {
         EpiException::raise(new EpiConfigException("Config file ({$file}) does not exist"));
