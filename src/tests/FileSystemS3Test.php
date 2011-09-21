5f5e5097b35881e147a08f0565d3d4286bf1e5e1
Jaisen Mathai <jaisen@jmathai.com>
2011-09-21 00:08:25 -0700

    Finished unit tests for the S3 adapter.
    Closes Gh-116

--- src/tests/FileSystemS3Test.php
+++ src/tests/FileSystemS3Test.php
@@ -21,7 +21,7 @@ class FileSystemS3Test extends PHPUnit_Framework_TestCase
 
     $this->s3Stub->expects($this->any())
       ->method('batch')
-      ->will($this->returnValue(new batchResponseSuccess));
+      ->will($this->returnValue(new BatchResponseSuccess));
     $fs = getFs();
     $fs->inject('fs', $this->s3Stub);
 
@@ -39,12 +39,36 @@ class FileSystemS3Test extends PHPUnit_Framework_TestCase
 
     $this->s3Stub->expects($this->any())
       ->method('batch')
-      ->will($this->returnValue(new batchResponseFailure));
+      ->will($this->returnValue(new BatchResponseFailure));
     $fs = getFs();
     $fs->inject('fs', $this->s3Stub);
 
     $res = $fs->deletePhoto('foo');
     $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for deletePhoto');
+  }
+
+  public function testGetPhotoSuccess()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('get_object')
+      ->will($this->returnValue(new SuccessResponse));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->getPhoto('foo');
+    $this->assertTrue($res !== false, 'The S3 FileSystem adapter did not return TRUE for getPhoto');
+  }
+
+  public function testGetPhotoFailure()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('get_object')
+      ->will($this->returnValue(new FailureResponse));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->getPhoto('foo');
+    $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for getPhoto');
   }
 
   public function testPutPhotoSuccess()
@@ -69,6 +93,132 @@ class FileSystemS3Test extends PHPUnit_Framework_TestCase
     $fs->inject('fs', $this->s3Stub);
     $res = getFs()->putPhoto('foo', 'bar');
     $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for putPhoto');
+  }
+
+  public function testPutPhotosSuccess()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('batch')
+      ->will($this->returnValue(new BatchResponseSuccess));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = getFs()->putPhotos(array(array('local1' => 'remote1'), array('local2' => 'remote2')), 'bar');
+    $this->assertTrue($res, 'The S3 FileSystem adapter did not return TRUE for putPhotos');
+  }
+
+  public function testPutPhotosFailure()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('batch')
+      ->will($this->returnValue(new BatchResponseFailure));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = getFs()->putPhotos(array(array('local1' => 'remote1'), array('local2' => 'remote2')), 'bar');
+    $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for putPhotos');
+  }
+
+  public function testGetHost()
+  {
+    $host = getFs()->getHost();
+    $this->assertTrue(!empty($host), 'unittest', 'The S3 FileSystem adapter did not return "unittest" as the host');
+  }
+
+  public function testInitializeInvalidBucketNameCreateFailure()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_create')
+      ->will($this->returnValue(false));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->initialize();
+    $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for initializing an invalid bucket name');
+  }
+
+  public function testInitializeInvalidBucketNameSupportFailure()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_create')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_support')
+      ->will($this->returnValue(false));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->initialize();
+    $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for initializing an invalid bucket name');
+  }
+
+  public function testInitializeCreateBucketFailure()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_create')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_support')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('get_bucket_list')
+      ->will($this->returnValue(array()));
+    $this->s3Stub->expects($this->any())
+      ->method('create_bucket')
+      ->will($this->returnValue(new FailureResponse));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->initialize();
+    $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for initializing an bucket creation failure');
+  }
+
+  public function testInitializeCreateBucketFailedSettingPolicy()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_create')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_support')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('get_bucket_list')
+      ->will($this->returnValue(array()));
+    $this->s3Stub->expects($this->any())
+      ->method('create_bucket')
+      ->will($this->returnValue(new SuccessResponse));
+    $this->s3Stub->expects($this->any())
+      ->method('set_bucket_policy')
+      ->will($this->returnValue(new FailureResponse));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->initialize();
+    $this->assertFalse($res, 'The S3 FileSystem adapter did not return FALSE for initializing an bucket and failure setting the policy');
+  }
+
+  public function testInitializeCreateBucketSuccess()
+  {
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_create')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('validate_bucketname_support')
+      ->will($this->returnValue(true));
+    $this->s3Stub->expects($this->any())
+      ->method('get_bucket_list')
+      ->will($this->returnValue(array()));
+    $this->s3Stub->expects($this->any())
+      ->method('create_bucket')
+      ->will($this->returnValue(new SuccessResponse));
+    $this->s3Stub->expects($this->any())
+      ->method('set_bucket_policy')
+      ->will($this->returnValue(new SuccessResponse));
+
+    $fs = getFs();
+    $fs->inject('fs', $this->s3Stub);
+    $res = $fs->initialize();
+    $this->assertTrue($res, 'The S3 FileSystem adapter did not return TRUE for initializing an bucket and setting the policy');
   }
 }
 
@@ -110,6 +260,11 @@ class BatchResponseSuccess
   {
     return new SuccessResponse;    
   }
+
+  public function create_object()
+  {
+    return new SuccessResponse;    
+  }
 }
 
 class BatchResponseFailure
@@ -120,6 +275,11 @@ class BatchResponseFailure
   }
 
   public function send()
+  {
+    return new FailureResponse;    
+  }
+
+  public function create_object()
   {
     return new FailureResponse;    
   }
