d2987e3a2293e658ea032471249786c10e29cf90
Jaisen Mathai <jaisen@jmathai.com>
2011-10-07 15:20:10 -0700

    Finalizing the mobile markup/js for the theme. Closes Gh-197

--- src/html/assets/themes/default/templates/template-mobile.php
+++ src/html/assets/themes/default/templates/template-mobile.php
@@ -25,76 +25,17 @@
     <?php echo $body; ?>
     <script type="text/javascript" src="<?php getTheme()->asset(getConfig()->get('dependencies')->javascript); ?>"></script>
     <script type="text/javascript" src="<?php getTheme()->asset('util'); ?>"></script>
-    <script type="text/javascript" src="<?php getTheme()->asset('javascript', 'jquery.mobile.js'); ?>"></script>
-    <script type="text/javascript" src="<?php getTheme()->asset('javascript', 'ps.js'); ?>"></script>
     <script>
-      /*
-       * IMPORTANT!!!
-       * REMEMBER TO ADD  rel="external"  to your anchor tags. 
-       * If you don't this will mess with how jQuery Mobile works
-       */
-      
-      (function(window, $, PhotoSwipe){
-        console.log('yes');
-        $(document).ready(function(){
-        console.log('ready');
-          $('div.gallery-page').live('pageshow', function(e){
-        console.log('show');
-              
-            // See if there is a PhotoSwipe instance associated with the page.
-            // For this demo I've assumed one page has one instance and the ID 
-            // for each instance is the same as the page ID.
-            //
-            // Of course, it's up to you how many instances per page and what
-            // ID naming convention you use!
-            var 
-              currentPage = $(e.target),
-              photoSwipeInstanceId = currentPage.attr('id'),
-              photoSwipeInstance = PhotoSwipe.getInstance(photoSwipeInstanceId)
-              options = {};
-            
-            if (typeof photoSwipeInstance === "undefined" || photoSwipeInstance === null) {
-              photoSwipeInstance = $("ul.gallery a", e.target).photoSwipe(options, photoSwipeInstanceId);
-            }
-            
-            return true;
-            
-          });
-            
-        });
-      
-      }(window, window.jQuery, window.Code.PhotoSwipe));
-    </script>
-    <!--<script>
         OP.Util.init(jQuery, {
-          js: {
-            assets: [
-              '<?php getTheme()->asset('javascript', 'jquery.mobile.js'); ?>',
-              '<?php getTheme()->asset('javascript', 'ps.js'); ?>'
-            ],
-            onComplete: function(){
-              $('div.gallery-page').live('pageshow', function(e){
-                  
-                // See if there is a PhotoSwipe instance associated with the page.
-                // For this demo I've assumed one page has one instance and the ID 
-                // for each instance is the same as the page ID.
-                //
-                // Of course, it's up to you how many instances per page and what
-                // ID naming convention you use!
-                var 
-                  currentPage = $(e.target),
-                  photoSwipeInstanceId = currentPage.attr('id'),
-                  photoSwipeInstance = PhotoSwipe.getInstance(photoSwipeInstanceId)
-                  options = {};
-                
-                if (typeof photoSwipeInstance === "undefined" || photoSwipeInstance === null) {
-                  photoSwipeInstance = $("ul.gallery a", e.target).photoSwipe(options, photoSwipeInstanceId);
-                }
-                return true;
-              });
+            js: {
+                assets: [
+                    '<?php getTheme()->asset('javascript', 'jquery.mobile.js'); ?>',
+                    '<?php getTheme()->asset('javascript', 'ps.js'); ?>',
+                    '<?php getTheme()->asset('javascript', 'openphoto-theme-mobile.js'); ?>'
+                ],
+                onComplete: function(){ opTheme.init.attach(window.Code.PhotoSwipe); }
             }
-          }
         });
-    </script>-->
+    </script>
 </body>
 </html>
