3f78a92b5aabd254a68dc2f5fa421d8110036f5e
Jaisen Mathai <jaisen@jmathai.com>
2011-10-07 11:23:04 -0700

    Adding more functionality to mobile including tags page and search. Gh-197

--- src/html/assets/themes/default/templates/front-mobile.php
+++ src/html/assets/themes/default/templates/front-mobile.php
@@ -2,16 +2,16 @@
 	<div data-role="header" class="photoheader" data-theme="c"></div>
 
 	<div data-role="content">	
-    <form id="search-photo-form" action="ajax-gallery1.html" method="get">
+    <form id="search-photo-form" action="/photos/list" method="get">
             <div data-role="fieldcontain" class="center-wrapper">
-                <input type="search" name="search-photos" id="search-photos" value="search your photos ..."/>
+                <input type="search" name="tags" id="tags" placeholder="search your photos ..."/>
             </div>
         </form>		
     <ul data-role="listview" data-inset="true">
         <li><a href="/photos/list"><img
         src="<?php getTheme()->asset('image', 'header-navigation-photos.png'); ?>" alt="my photos"
                         class="ui-li-icon">Photos</a></li> 
-        <li><a href="#"><img
+        <li><a href="/tags/list"><img
         src="<?php getTheme()->asset('image', 'header-navigation-tags.png'); ?>" alt="tags"
                         class="ui-li-icon">Tags</a></li>
     </ul> 
