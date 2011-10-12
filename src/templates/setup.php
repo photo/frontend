f1fb73c2fed5d85dfd5db50958441f35c56f474e
Kevin Hornschemeier <khornschemeier@gmail.com>
2011-10-11 20:16:20 -0400

    Added logic to only show the setup cancel button when rerunning

--- src/templates/setup.php
+++ src/templates/setup.php
@@ -22,7 +22,7 @@
       <label for="email">Email address</label>
       <input type="text" name="email" id="email" <?php if(isset($email)) { ?>value="<?php Utility::safe($email); ?>"<?php } ?> data-validation="required email">
       <input type="hidden" name="appId" id="appId" <?php if(isset($appId)) { ?>value="<?php Utility::safe($appId); ?>"<?php } ?>>
-      <button type="button" onclick="window.location = '/'">Cancel</button>
+      <?php if(isset($_GET['edit'])) { ?><button type="button" onclick="window.location = '/'">Cancel</button><?php } ?>
       <button type="submit">Continue to Step 2</button>
     </form>
   </div>
@@ -47,7 +47,7 @@
         <option value="S3"<?php echo ($filesystem == 'S3') ? ' selected="selected"' : '' ?>>Amazon S3</option>
         <option value="LocalFs"<?php echo ($filesystem == 'LocalFs') ? ' selected="selected"' : '' ?>>Local filesystem</option>
       </select>
-      <button type="button" onclick="window.location = '/'">Cancel</button>
+      <?php if(isset($_GET['edit'])) { ?><button type="button" onclick="window.location = '/'">Cancel</button><?php } ?>
       <button type="submit">Continue to Step 3</button>
     </form>
   </div>
@@ -97,7 +97,7 @@
           <label for="fsHost">File system hostname for download URL</label>
         <input type="text" name="fsHost" id="fsHost" size="50" data-validation="required" value="<?php echo $fsHost; ?>">
       <?php } ?>
-      <button type="button" onclick="window.location = '/'">Cancel</button>
+      <?php if(isset($_GET['edit'])) { ?><button type="button" onclick="window.location = '/'">Cancel</button><?php } ?>
       <button type="submit">Complete setup</button>
     </form>
   </div>
