dfdd102c569a5e76d8e59066fcff3c38d37a4be4
Jaisen Mathai <jaisen@jmathai.com>
2011-10-16 15:07:09 -0700

    Adding first optional theme.

---
+++ src/html/assets/themes/beisel/templates/tags-mobile.php
@@ -0,0 +1,23 @@
+<div data-role="page" data-add-back-btn="true" id="Gallery1" class="gallery-page" data-theme="c">
+  <div data-role="header" class="photoheader" data-theme="c">
+    <a href="/" data-icon="home" class="ui-btn-right" rel="external">Home</a>
+  </div>
+	<div data-role="content">	
+    <?php if(empty($tags)) { ?>
+      Sorry, no photos have been tagged.
+    <?php } else { ?>
+      <ul data-role="listview" data-inset="true">
+        <?php foreach($tags as $tag) { ?>
+          <li class="size-<?php Utility::safe($tag['weight']); ?>">
+            <a href="<?php Url::photosView("tags-{$tag['id']}"); ?>" title="<?php Utility::safe($tag['count']); ?> photos">
+              <?php Utility::safe($tag['id']); ?>
+            </a>
+          </li>
+        <?php } ?>
+      </ol>
+    <?php } ?>
+  </div>
+	<div data-role="footer" data-theme="c">
+    <h4>The OpenPhoto Project &#169; <?php echo date('Y'); ?></h4>
+	</div>
+</div>
