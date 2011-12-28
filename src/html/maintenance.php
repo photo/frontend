0c67250458fc9f138ff7c41ad1e03a225b4eea6a
Jaisen Mathai <jaisen@jmathai.com>
2011-12-27 18:59:44 -0800

    Adding maintenance page.

---
+++ src/html/maintenance.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="language" content="english,en" />
+  <meta name="distribution" content="global" />
+    <title>OpenPhoto</title>
+    <meta name="description" content="OpenPhoto - A free, hosted, portable and open source photo sharing service">
+    <meta name="keywords" content="OpenPhoto, photos, photo">
+    <meta name="author" content="openphoto.me">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/assets/themes/beisel/images/favicon.png">
+    <link rel="apple-touch-icon" href="/assets/themes/beisel/images/apple-touch-icon.png">
+    <link rel="stylesheet" href="/assets/themes/beisel/stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/themes/beisel/stylesheets/main.css">
+    <style type="text/css">
+      #wrapper{
+        margin-top:200px;
+      }
+      #logo{
+        width:181px;
+        height:35px;
+        margin:auto;
+      }
+      .words{
+        line-height:27px;
+        font-size:30px;
+      }
+      .words .light{
+        color:#555658;
+      }
+    </style>
+  </head>
+
+<body class="front">
+  <div id="wrapper" class="container">
+    <div class="row">
+      <div class="span3 offset2">
+        <img src="/assets/images/logo.png" id="logo">
+      </div>
+      <div class="span10 words">
+        | currently <span class="light">undergoing</span> maintenance
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+
