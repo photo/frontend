1a6ec56eb60d0620c286415a10e8dfc6ad137bba
Jaisen Mathai <jaisen@jmathai.com>
2012-01-25 16:45:17 -0800

    Adding unit tests for UserConfig model. Closes #468

--- src/libraries/models/UserConfig.php
+++ src/libraries/models/UserConfig.php
@@ -1,12 +1,22 @@
 <?php
 class UserConfig
 {
-  private $basePath, $host;
+  protected $utility, $basePath, $host;
 
-  public function __construct()
+  public function __construct($params = null)
   {
+    if(isset($params['config']))
+      $this->config = $params['config'];
+    else
+      $this->config = getConfig();
+
+    if(isset($params['utility']))
+      $this->utility = $params['utility'];
+    else
+      $this->utility = new Utility;
+
     $this->basePath = dirname(dirname(dirname(__FILE__)));
-    $this->host = $_SERVER['HTTP_HOST'];
+    $this->host = $_SERVER['HTTP_HOST']; // TODO this isn't the best idea
   }
 
   public function getSiteSettings()
@@ -19,12 +29,11 @@ class UserConfig
 
   public function writeSiteSettings($settings)
   {
-    $utilityObj = new Utility;
     $configFile = $this->getConfigFile();
     if(!$configFile)
       return false;
 
-    $iniString = $utilityObj->generateIniString($settings, true);
+    $iniString = $this->utility->generateIniString($settings, true);
     if(!$iniString || empty($iniString))
       return false;
 
@@ -33,7 +42,7 @@ class UserConfig
 
   public function load()
   {
-    getConfig()->load('defaults.ini');
+    $this->config->load('defaults.ini');
     $configFile = $this->getConfigFile();
 
     // backwards compatibility for 1.2.1 -> 1.2.2 upgrade
@@ -41,17 +50,16 @@ class UserConfig
 
     if($configFile)
     {
-      getConfig()->load($configFile);
-      if(file_exists(sprintf('%s/override.ini', getConfig()->get('paths')->configs)))
-        getConfig()->load('override.ini');
+      $this->config->load($configFile);
+      if(file_exists(sprintf('%s/override.ini', $this->config->get('paths')->configs)))
+        $this->config->load('override.ini');
 
       // we need to load the deps to get the theme modules
-      require getConfig()->get('paths')->libraries . '/dependencies.php';
+      require $this->config->get('paths')->libraries . '/dependencies.php';
 
-      getConfig()->load(sprintf('%s/html/assets/themes/%s/config/settings.ini', dirname(dirname(dirname(__FILE__))), getTheme()->getThemeName()));
-      $utilityObj = new Utility;
-      if($utilityObj->isMobile() && file_exists($mobileSettings = sprintf('%s/html/assets/themes/%s/config/settings-mobile.ini', dirname(dirname(dirname(__FILE__))), getTheme(false)->getThemeName())))
-        getConfig()->load($mobileSettings);
+      $this->config->load(sprintf('%s/html/assets/themes/%s/config/settings.ini', dirname(dirname(dirname(__FILE__))), getTheme()->getThemeName()));
+      if($this->utility->isMobile() && file_exists($mobileSettings = sprintf('%s/html/assets/themes/%s/config/settings-mobile.ini', dirname(dirname(dirname(__FILE__))), getTheme(false)->getThemeName())))
+        $this->config->load($mobileSettings);
 
       return true;
     }
@@ -61,9 +69,6 @@ class UserConfig
   private function getConfigFile()
   {
     $configFile = sprintf('%s/userdata/configs/%s.ini', $this->basePath, $this->host);
-    if(!file_exists($configFile))
-      $configFile = sprintf('%s/generated/%s.ini', Epi::getPath('config'), $this->host);
-
     if(!file_exists($configFile))
       return false;
     return $configFile;
