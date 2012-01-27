4998ac2810bfa3b29c6bfa98994b5542fc9a5bcb
Jaisen Mathai <jaisen@jmathai.com>
2012-01-27 15:16:07 -0800

    Adjusting permission error messaging to differentiate between not owner and not logged in. #447

--- src/html/assets/themes/beisel/templates/error403.php
+++ src/html/assets/themes/beisel/templates/error403.php
@@ -1,2 +1,6 @@
 <img src="<?php echo $this->theme->asset('image', 'error.png'); ?>">
-<h1>Sorry, but you don't have permission to view this page.</h1>
+<?php if($this->user->isLoggedIn() && !$this->user->isOwner()) { ?>
+  <h1>Sorry, but you don't have permission to view this page.</h1>
+<?php } else { ?>
+  <h1>You need to be logged in to view this page.</h1>
+<?php } ?>
