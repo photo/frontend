c6d4272eabc2a3d85813e6728938cfd88c7fd53e
Jaisen Mathai <jaisen@jmathai.com>
2012-03-04 22:39:16 -0800

    Using $routeObj over getRoute(). Closes #541

--- src/libraries/routes-error.php
+++ src/libraries/routes-error.php
@@ -5,14 +5,14 @@
  * Everything in []'s are optional
  * /assets/cache/:version/:type/:file[,:file]
  */
-getRoute()->get('/assets/cache/[^/]+/(js|css)/(c|m)(/.+)', array('AssetsController', 'get'));
+$routeObj->get('/assets/cache/[^/]+/(js|css)/(c|m)(/.+)', array('AssetsController', 'get'));
 
 /*
  * Error endpoints
  * All error endpoints follow the same convention.
  * /error/{code}
  */
-getRoute()->get('/error/403', array('GeneralController', 'error403'));
-getRoute()->get('/error/404', array('GeneralController', 'error404'));
-getRoute()->get('/error/500', array('GeneralController', 'error500'));
-getRoute()->get('.*', array('GeneralController', 'error404'));
+$routeObj->get('/error/403', array('GeneralController', 'error403'));
+$routeObj->get('/error/404', array('GeneralController', 'error404'));
+$routeObj->get('/error/500', array('GeneralController', 'error500'));
+$routeObj->get('.*', array('GeneralController', 'error404'));
