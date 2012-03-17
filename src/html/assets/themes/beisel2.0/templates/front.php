0dc95cde05a63c657ebb51de87c8306ed154584a
Jaisen Mathai <jaisen@jmathai.com>
2012-03-17 15:59:31 -0700

    Adding tracking JS for carousel thumbnails. #563

--- src/html/assets/themes/beisel2.0/templates/front.php
+++ src/html/assets/themes/beisel2.0/templates/front.php
@@ -37,32 +37,32 @@
         <div id="homeCarousel" class="row hero-unit carousel front">
           <!-- large photo -->
           <div class="carousel-inner">
-            <?php for($i=0; $i<5 && $i<$photos[0]['totalRows']; $i++) { ?>
-              <div class="item <?php if($i == 0) { ?>active<?php } ?> row">
-                <a href="<?php $this->url->photoView($photos[$i]['id']); ?>" class="span9"><img src="<?php $this->url->photoUrl($photos[$i], '870x652xCR'); ?>" alt="<?php $this->utility->safe($photos[$i]['title']); ?>" /></a>
+            <?php foreach($photos as $i => $photo) { ?>
+              <div class="item <?php if($i == 0) { ?>active<?php } ?> row" data-index="<?php echo $i; ?>">
+                <a href="<?php $this->url->photoView($photo['id']); ?>" class="span9"><img src="<?php $this->url->photoUrl($photo, '870x652xCR'); ?>" alt="<?php $this->utility->safe($photo['title']); ?>" /></a>
                 <div class="span3">
                   <h1>
-                    <a href="<?php $this->url->photoView($photos[$i]['id']); ?>">
-                      <?php if(empty($photos[$i]['title'])) { ?>
-                        <?php $this->utility->safe($photos[$i]['filenameOriginal']); ?>
+                    <a href="<?php $this->url->photoView($photo['id']); ?>">
+                      <?php if(empty($photo['title'])) { ?>
+                        <?php $this->utility->safe($photo['filenameOriginal']); ?>
                       <?php } else { ?>
-                        <?php $this->utility->safe($photos[$i]['title']); ?>
+                        <?php $this->utility->safe($photo['title']); ?>
                       <?php } ?>
                     </a>
                   </h1>
-                  <p>This photo was taken <?php $this->utility->safe($this->utility->timeAsText($photos[$i]['dateTaken'])); ?></p>
-                  <?php if(isset($photos[$i]['longitude']) && !empty($photos[$i]['longitude'])) { ?>
-                    <div class="map"><img src="<?php $this->utility->staticMapUrl($photos[$i]['latitude'], $photos[$i]['longitude'], 10, '250x100'); ?>"></div>
-                    <a href="" class="invert"><i class="icon-map-marker"></i> <?php printf('%s, %s', $this->utility->safe($photos[$i]['longitude'], false), $this->utility->safe($photos[$i]['latitude'], false)); ?></a><br/>
+                  <p>This photo was taken <?php $this->utility->safe($this->utility->timeAsText($photo['dateTaken'])); ?></p>
+                  <?php if(isset($photo['longitude']) && !empty($photo['longitude'])) { ?>
+                    <div class="map"><img src="<?php $this->utility->staticMapUrl($photo['latitude'], $photo['longitude'], 10, '250x100'); ?>"></div>
+                    <a href="" class="invert"><i class="icon-map-marker"></i> <?php printf('%s, %s', $this->utility->safe($photo['longitude'], false), $this->utility->safe($photo['latitude'], false)); ?></a><br/>
                   <?php } ?>
                   <!--<a href="" class="invert"><i class="icon-heart"></i> 4 favorites</a><br />-->
                   <a href="" class="invert"><i class="icon-comment"></i> <?php if(!isset($photos['actions'])) { ?>0<?php } else { $this->utility->safe(count($photos['actions'])); } ?> comments &amp; favorites</a><br />
                   <!--<a href="" class="invert"><i class="icon-signal"></i> 154 visits</a><br />
                   <a href="" class="invert"><i class="icon-folder-close"></i> path photos</a>-->
                   <br /><br />
-                  <?php if(count($photos[$i]['tags']) > 0) { ?>
+                  <?php if(count($photo['tags']) > 0) { ?>
                     <p>
-                      <?php foreach($photos[$i]['tags'] as $tag) { ?>
+                      <?php foreach($photo['tags'] as $tag) { ?>
                         <span class="label label-tag">
                           <a href="<?php $this->url->photosView(sprintf('tags-%s', $this->utility->safe($tag, false))); ?>"><?php $this->utility->safe($tag); ?></a>
                         </span>
@@ -71,6 +71,7 @@
                   <?php } ?>
                 </div>
               </div>
+              <?php if($photos[0]['totalRows'] < 12 && $i >= 6) break; ?>
             <?php } ?>
           </div>
           <div class="carousel-control">
@@ -78,18 +79,18 @@
             <a class="pause btn" href="#homeCarousel" data-slide="pause"><i class="icon-pause icon-large"></i></a>
             <a class="left btn" href="#homeCarousel" data-slide="prev"><i class="icon-chevron-left icon-large"></i></a>
           </div>
-          </div>
         </div>
 
-        <?php if($photos[0]['totalRows'] > 5) { ?>
+        <?php if($photos[0]['totalRows'] > 0) { ?>
           <!-- thumbnails -->
           <ul class="thumbnails carouselthumbs">
-            <?php for($i=5; $i<11 && $i<$photos[0]['totalRows']; $i++) { ?>
+            <?php foreach($photos as $i => $photo) { ?>
               <li class="span2">
-                <a href="<?php $this->url->photoView($photos[$i]['id']); ?>" class="thumbnail <?php if($i == 0) { ?>active<?php } ?>">
-                  <img src="<?php $this->url->photoUrl($photos[$i], '870x652xCR'); ?>" alt="<?php $this->utility->safe($photos[$i]['title']); ?>" />
+                <a href="<?php $this->url->photoView($photo['id']); ?>" class="thumbnail <?php if($i == 0) { ?>active<?php } ?>">
+                  <img src="<?php $this->url->photoUrl($photo, '870x652xCR'); ?>" alt="<?php $this->utility->safe($photo['title']); ?>" />
                 </a>
               </li>
+              <?php if($photos[0]['totalRows'] < 12 && $i >= 6) break; ?>
             <?php } ?>
           </ul>
         <?php } ?>
