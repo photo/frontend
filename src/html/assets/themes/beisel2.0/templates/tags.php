fa6cb39a05bcc8c2a99d5528e1c09cdf6c988d25
Jaisen Mathai <jaisen@jmathai.com>
2012-03-18 21:27:15 -0700

    Styles for the tags page. #563

--- src/html/assets/themes/beisel2.0/templates/tags.php
+++ src/html/assets/themes/beisel2.0/templates/tags.php
@@ -3,23 +3,15 @@
     <h1>Tags</h1>
     <div class="subnav">
       <ul class="nav nav-pills">
-        <li><a href="#"><i class="icon-arrow-down icon-large"></i> A-Z</a></li>
-        <li><a href="#"><i class="icon-arrow-up icon-large"></i> Z-A</a></li>
-        <li class="last">
-          <form action="">
-            <div class="input-append">
-              <input class="search-query span2" id="appendedInput" size="16" type="text" data-provide="typeahead" />
-              <a href="" class="add-on"><i class="icon-search icon-large"></i></a>
-            </div>
-          </form>
-        </li>
+        <li><a href="/tags/list"><i class="icon-arrow-down icon-large"></i> A-Z</a></li>
+        <li><a href="/tags/list?sortBy=id,desc"><i class="icon-arrow-up icon-large"></i> Z-A</a></li>
       </ul>
     </div>
   </header>
 
   <div class="row hero-unit tag-list <?php if(empty($tags)) { ?>empty<?php } ?>">
     <?php foreach($tags as $tag) { ?>
-      <span class="label label-tag">
+      <span class="label label-tag tag-<?php $this->utility->safe($tag['weight']); ?>">
         <a href="<?php $this->url->photosView(sprintf('tags-%s', $this->utility->safe($tag['id'], false))); ?>"><?php $this->utility->safe($tag['id']); ?></a>
       </span>
     <?php } ?>
