3e72b8899cbe3a86bc2eb799bbe47eee8d429dfb
Jaisen Mathai <jaisen@jmathai.com>
2012-04-02 00:36:58 -0700

    Fixing empty next or previous links. #563

--- src/html/assets/themes/beisel2.0/templates/photo-details.php
+++ src/html/assets/themes/beisel2.0/templates/photo-details.php
@@ -70,10 +70,20 @@
         <div class="morephotos">
           <strong>Discover more photos:</strong>
           <ul>
-            <li class="buttonmp"><form action="<?php $this->url->photoView($photo['previous']['id'], $options); ?>"><button type="submit" class="btn btn-primary"><i class="icon-arrow-left"></i></button></form></li>
-            <li class="span1 mspacer"><a href="<?php $this->url->photoView($photo['previous']['id'], $options); ?>"><img src="<?php $this->url->photoUrl($photo['previous'], $this->config->photoSizes->nextPrevious); ?>" alt="Previous photo: <?php $this->utility->safe($photo['previous']['title']); ?>" class="photo-view-click previous-photo"/></a></li>
-            <li class="span1"><a href="<?php $this->url->photoView($photo['next']['id'], $options); ?>"><img src="<?php $this->url->photoUrl($photo['next'], $this->config->photoSizes->nextPrevious); ?>" alt="Next photo: <?php $this->utility->safe($photo['previous']['title']); ?>" class="photo-view-click next-photo" /></a></li>
-            <li class="buttonmp"><form action="<?php $this->url->photoView($photo['next']['id'], $options); ?>"><button type="submit" class="btn btn-primary"><i class="icon-arrow-right"></i></button></form></li>
+            <?php if(!empty($photo['previous'])) { ?>
+              <li class="buttonmp"><form action="<?php $this->url->photoView($photo['previous']['id'], $options); ?>"><button type="submit" class="btn btn-primary"><i class="icon-arrow-left"></i></button></form></li>
+              <li class="span1 mspacer"><a href="<?php $this->url->photoView($photo['previous']['id'], $options); ?>"><img src="<?php $this->url->photoUrl($photo['previous'], $this->config->photoSizes->nextPrevious); ?>" alt="Previous photo: <?php $this->utility->safe($photo['previous']['title']); ?>" class="photo-view-click previous-photo"/></a></li>
+            <?php } else { ?>
+              <li class="buttonmp"><button type="button" class="btn disabled"><i class="icon-arrow-left"></i></button></li>
+              <li class="span1 mspacer">&nbsp;</li>
+            <?php } ?>
+            <?php if(!empty($photo['next'])) { ?>
+              <li class="span1"><a href="<?php $this->url->photoView($photo['next']['id'], $options); ?>"><img src="<?php $this->url->photoUrl($photo['next'], $this->config->photoSizes->nextPrevious); ?>" alt="Next photo: <?php $this->utility->safe($photo['previous']['title']); ?>" class="photo-view-click next-photo" /></a></li>
+              <li class="buttonmp"><form action="<?php $this->url->photoView($photo['next']['id'], $options); ?>"><button type="submit" class="btn btn-primary"><i class="icon-arrow-right"></i></button></form></li>
+            <?php } else { ?>
+              <li class="span1">&nbsp;</li>
+              <li class="buttonmp"><button type="button" class="btn disabled"><i class="icon-arrow-right"></i></button></li>
+            <?php } ?>
           </ul>
           <hr />
         </div>
