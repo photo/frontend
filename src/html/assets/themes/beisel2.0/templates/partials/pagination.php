883c6dd9d7374b3f679cf48a5fe49a32669c51f5
Jaisen Mathai <jaisen@jmathai.com>
2012-04-02 20:28:43 -0700

    Fixing pagination bug. #563

--- src/html/assets/themes/beisel2.0/templates/partials/pagination.php
+++ src/html/assets/themes/beisel2.0/templates/partials/pagination.php
@@ -5,7 +5,7 @@
         <li <?php if($page == $currentPage) { ?>class="active"<?php } ?>>
           <?php if(strstr($requestUri, '?') === false) { ?>
             <a href="<?php $this->utility->safe(sprintf('%s?page=%d', $requestUri, $page)); ?>">
-          <?php } elseif(strstr($requestUri, '/list') !== false) { ?>
+          <?php } elseif(strstr($requestUri, '/list') !== false && !isset($_GET['page'])) { ?>
             <a href="<?php $this->utility->safe(preg_replace('#(/page-\d+)?/list#', "/page-{$page}/list", $requestUri)); ?>">
           <?php } else { ?>
             <a href="<?php $this->utility->safe(sprintf('%s&page=%d', preg_replace('#&?page=\d+#', '', $requestUri), $page)); ?>">
