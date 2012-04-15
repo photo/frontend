b0b0401e261d610ce1cd5be9113f6f20084dee84
Jaisen Mathai <jaisen@jmathai.com>
2012-04-14 22:04:21 -0700

    Only show padlock if private. Closes #599

--- src/html/assets/themes/beisel2.0/templates/photo-details.php
+++ src/html/assets/themes/beisel2.0/templates/photo-details.php
@@ -23,7 +23,9 @@
       <?php  } ?>
       <div class="span9">
         <div class="photodetailpos">
-          <div class="private" title="private"><i class="icon-lock icon-large"></i></div>
+          <?php if($photo['permission'] == 0) { ?>
+            <div class="private" title="private"><i class="icon-lock icon-large"></i></div>
+          <?php } ?>
           <img class="photo" style="width:<?php $this->utility->safe($photo['photo'.$this->config->photoSizes->detail][1]); ?>px; height:<?php $this->utility->safe($photo['photo'.$this->config->photoSizes->detail][2]); ?>px;" src="<?php $this->url->photoUrl($photo, $this->config->photoSizes->detail); ?>" alt="<?php $this->utility->safe($photo['title']); ?>">
         </div>
         <a name="comment-form"></a>
