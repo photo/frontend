e67529bd7363993ebe95d13addd30ef04cc8f1c6
till <till@php.net>
2012-04-22 16:56:38 +0200

    * remove phpstorm header

--- src/tests/helpers/vfs.php
+++ src/tests/helpers/vfs.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: till
- * Date: 4/22/12
- * Time: 12:28 AM
- * To change this template use File | Settings | File Templates.
- */
 $paths = (array)explode(PATH_SEPARATOR, ini_get('include_path'));
 foreach($paths as $path)
 {
