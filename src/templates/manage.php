63bc1a3cc7a4eab1fb5de4c38116349d8622596c
Jaisen Mathai <jaisen@jmathai.com>
2012-04-25 12:46:23 -0700

    Making the manage photos page more clear with descriptions and more accurate text. #608

--- src/templates/manage.php
+++ src/templates/manage.php
@@ -2,6 +2,18 @@
 
   <?php echo $navigation; ?>
 
+  <div class="row hero-unit blurb">
+    <h2>Tips on updating your photos</h2>
+    <p>
+      You can easily edit title, tags, permission and licensing of your photos. 
+      When you hover over a photo you'll see two icons.
+      <ol>
+        <li>The <i class="icon-edit icon-large"></i> <em>pencil</em> icon brings up a dialog to edit that photo.</li>
+        <li>The <i class="icon-ok icon-large"></i> <em>checkmark</em> queues that photo in case you wanted to update multiple photos at once.</li>
+      </ol>
+    </p>
+  </div>
+
   <?php if($photos[0]['totalRows'] > 0) { ?>
     <?php echo $pagination; ?>
     <ul class="thumbnails">
