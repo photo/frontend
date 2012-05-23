3e3514ac509f3b6dfa84859b440186cac551fe99
Jaisen Mathai <jaisen@jmathai.com>
2012-05-23 15:02:44 -0700

    Updating minified JS and cache key. #722

--- src/html/assets/themes/beisel2.0/templates/template.php
+++ src/html/assets/themes/beisel2.0/templates/template.php
@@ -162,7 +162,7 @@
             '<?php $this->utility->safe($this->config->site->cdnPrefix);?><?php echo getAssetPipeline(true)->setMode(AssetPipeline::combined)->
                                                   addJs('/assets/javascripts/openphoto-helper.min.js')->
                                                   addJs($this->theme->asset('javascript', 'min/openphoto-theme-full.min.js', false))->
-                                                  getUrl(AssetPipeline::js, 'z'); ?>'
+                                                  getUrl(AssetPipeline::js, '0'); ?>'
             <?php } ?>
           ],
           onComplete: function(){ 
