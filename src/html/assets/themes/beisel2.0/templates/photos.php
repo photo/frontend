389dbd0d6cbd0f72aecaaccee2c0c67c0546d1fd
Jaisen Mathai <jaisen@jmathai.com>
2012-06-08 15:12:05 -0700

    Seed initial view of /photos/list with data from backend. #742

--- src/html/assets/themes/beisel2.0/templates/photos.php
+++ src/html/assets/themes/beisel2.0/templates/photos.php
@@ -7,29 +7,9 @@
         <li class="plain"><a>Showing photos between <i class="icon-calendar icon-large"></i> <span class="startdate date" data-time="<?php $this->utility->safe($minDate); ?>"><?php $this->utility->safe(date('l F jS, Y', $minDate)); ?></span> and <i class="icon-calendar icon-large"></i> <span class="enddate date" data-time="<?php $this->utility->safe($maxDate); ?>"><?php $this->utility->safe(date('l F jS, Y', $maxDate)); ?></span></a></li>
       </ul>
     </div>
-
-    <?php if(count($pages) > 1) { ?>
-      <div class="js-hide">
-        <?php $this->theme->display('partials/pagination.php', $pages); ?>
-      </div>
-    <?php } ?>
   <?php } ?>
+  <script> var initData = <?php echo json_encode($photos); ?>;</script>
   <div class="row hero-unit empty gallery">
-    <ul class="photo-grid js-hide">
-      <?php foreach($photos as $photo) { ?>
-        <li>
-          <div class="shell">
-            <a href="<?php $this->url->photoView($photo['id'], $options); ?>">
-              <img src="<?php $this->url->photoUrl($photo, $this->config->photoSizes->thumbnail); ?>" alt="<?php $this->utility->safe($photo['title']); ?>" class="thumb" />
-            </a>
-            <span class="meta">
-              <a href="" class="invert"><i class="icon-heart"></i>4x </a>
-              <a href="" class="invert"><i class="icon-tag"></i><?php echo count($photo['tags']); ?>x </a>
-            </span>
-          </div>
-        </li>
-      <?php } ?>
-    </ul>
     <div class="photo-grid-justify"></div>
     <br clear="all">
     <?php if($photos[0]['totalPages'] > 1) { ?>
