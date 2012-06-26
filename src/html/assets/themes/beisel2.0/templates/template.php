7f4ab17c3d81df1254c969429524f14008620a41
Jaisen Mathai <jaisen@jmathai.com>
2012-06-25 23:25:15 -0700

    Updating pin elements for batch photo editing. #787

--- src/html/assets/themes/beisel2.0/templates/template.php
+++ src/html/assets/themes/beisel2.0/templates/template.php
@@ -37,7 +37,7 @@
                                                                   addCss("/assets/stylesheets/upload.css")->
                                                                   addCss($this->theme->asset('stylesheet', 'chosen.css', false))->
                                                                   addCss($this->theme->asset('stylesheet', 'opme.css', false))->
-                                                                  getUrl(AssetPipeline::css, 'ac'); ?>">
+                                                                  getUrl(AssetPipeline::css, 'ad'); ?>">
       <!--[if IE 7]>
         <link rel="stylesheet" href="<?php $this->utility->safe($this->config->site->cdnprefix);?><?php echo getAssetPipeline(true)->addCss($this->theme->asset('stylesheet', 'font-awesome-ie7.css', false))->
                                                                   getUrl(AssetPipeline::css, 'a'); ?>">
@@ -176,7 +176,7 @@
             '<?php $this->utility->safe($this->config->site->cdnPrefix);?><?php echo getAssetPipeline(true)->setMode(AssetPipeline::combined)->
                                                   addJs('/assets/javascripts/openphoto-helper.min.js')->
                                                   addJs($this->theme->asset('javascript', 'min/openphoto-theme-full.min.js', false))->
-                                                  getUrl(AssetPipeline::js, 'ac'); ?>'
+                                                  getUrl(AssetPipeline::js, 'ad'); ?>'
             <?php } ?>
           ],
           onComplete: function(){ 
