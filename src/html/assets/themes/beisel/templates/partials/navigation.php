4c7075fd69215c67d328cffb064b570ea6f02d81
Jaisen Mathai <jaisen@jmathai.com>
2012-07-12 11:57:23 -0700

    Updating copy to say Mozilla Persona. Closes #817

--- src/html/assets/themes/beisel/templates/partials/navigation.php
+++ src/html/assets/themes/beisel/templates/partials/navigation.php
@@ -28,7 +28,7 @@
     <?php if($this->plugin->isActive('FacebookConnect')) { ?>
       <a class="login-click facebook" title="Signin using Facebook"><img src="<?php $this->theme->asset('image', 'facebook-icon.gif'); ?>" class="login-click facebook" hspace="5" /></a>
     <?php } ?>
-    <a class="login-click browserid" title="Signin using BrowserID"><img src="<?php $this->theme->asset('image', 'browserid.png'); ?>" class="login-click browserid" /></a>
+    <a class="login-click browserid" title="Signin using Mozilla Persona"><img src="<?php $this->theme->asset('image', 'browserid.png'); ?>" class="login-click browserid" /></a>
 	</li>
 <?php } ?>
 	<li id="nav-search" <?php if($this->utility->isActiveTab('search')) { ?> class="active" <?php } ?>>
