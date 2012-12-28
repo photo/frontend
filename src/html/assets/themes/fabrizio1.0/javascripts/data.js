ae36644421854d50e1ffaa7ba1df24d42ba7a166
Mark Fabrizio <markfabrizio@Marks-MacBook-Pro-2.local>
2012-12-28 18:37:11 -0500

    Inline editing on photo grid. Add x-editable library to fabrizio1.0 theme Modified setLocation function to ensure $tip is visible within the window

--- src/html/assets/themes/fabrizio1.0/javascripts/data.js
+++ src/html/assets/themes/fabrizio1.0/javascripts/data.js
@@ -68,13 +68,13 @@ if( !window.op.data ) window.op.data = {};
       this.model.on('change', this.modelChanged, this);
     },
     model: this.model,
-    tagName: 'span',
     className: 'profile-photo-meta',
     template    :_.template($('#profile-photo-meta').html()),
     render      :function(){
       $(this.el).html(this.template(this.model.toJSON()));
       return this;
     },
+    
     modelChanged: function() {
       this.render();
     }
@@ -127,6 +127,79 @@ if( !window.op.data ) window.op.data = {};
   var PhotoStore = new PhotoCollection({
     localStorage  :'op-photos'
   });
+  
+  /**
+   * Override the setPosition function to correct the x coordinate
+   * if it is tip extends beyond the width of the window on either side
+   */
+  $.fn.editableContainer.Constructor.prototype.setPosition = function(){
+    (function() {    
+        var $tip = this.tip()
+        , inside
+        , pos
+        , actualWidth
+        , actualHeight
+        , placement
+        , tp;
+
+        placement = typeof this.options.placement === 'function' ?
+        this.options.placement.call(this, $tip[0], this.$element[0]) :
+        this.options.placement;
+
+        inside = /in/.test(placement);
+       
+        $tip
+      //  .detach()
+      //vitalets: remove any placement class because otherwise they dont influence on re-positioning of visible popover
+        .removeClass('top right bottom left')
+        .css({ top: 0, left: 0, display: 'block' });
+      //  .insertAfter(this.$element);
+       
+        pos = this.getPosition(inside);
+
+        actualWidth = $tip[0].offsetWidth;
+        actualHeight = $tip[0].offsetHeight;
+
+        switch (inside ? placement.split(' ')[1] : placement) {
+            case 'bottom':
+                tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
+                break;
+            case 'top':
+                tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
+                break;
+            case 'left':
+                tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
+                break;
+            case 'right':
+                tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
+                break;
+        }
+        
+        // check that the x position is withinin the screen
+        if( tp.left < 0 ) {
+          var offset = tp.left-15;
+          tp.left = 5;
+          $tip.find('.arrow').css({'margin-left': offset });
+        }
+        else if( tp.left + actualWidth > $(window).width() ){
+          var offset = (tp.left+actualWidth) - ($(window).scrollLeft() + $(window).width());
+          tp.left -= (offset + 5);
+          $tip.find('.arrow').css({'margin-left': offset-5 });
+        }
+        else {
+          $tip.find('.arrow').css({'margin-left': -10 });
+        }
+        
+
+        $tip
+        .offset(tp)
+        .addClass(placement)
+        .addClass('in');
+        
+    }).call(this.container());
+  };
+  
+  
   var PhotoGalleryView = Backbone.View.extend({
     initialize: function() {
       this.model.on('change', this.modelChanged, this);
@@ -136,16 +209,64 @@ if( !window.op.data ) window.op.data = {};
     className: 'photo-meta',
     template    :_.template($('#photo-meta').html()),
     render      :function(){
-      $(this.el).html(this.template(this.model.toJSON()));
+      var self = this
+        , $el = $(this.el);
+      
+      // We need to capture the lastEditable because
+      // after a successful save, render is called to
+      // update the view. the $tip will be lost and the
+      // "hidden" event will not fire.
+      if( this.lastEditable ) {
+        this.lastEditable.hide();
+      }
+        
+      $el.html(this.template(this.model.toJSON()));
+      
+      // initialize the inline editor..
+      this.lastEditable = $el.find('.title a ').editable({
+        name: 'title',
+        placement: 'top',
+        title: 'Edit Title',
+        url: function(params){
+          
+          var d = new $.Deferred;
+          self.model.set(params.name, params.value, {silent:true});
+          self.model.save(null, {
+            success : function(){
+              d.done();
+            },
+            error : function(){
+              /**
+               * TODO: should report the error, but I'm not sure what the
+               * arguments / return values are.
+               */
+              d.reject();
+            }
+          });
+          return d;
+        }
+      }).on('shown', function(){
+        $el.parents('.imageContainer').addClass('editing');
+        $(this).data('editable').container.setPosition();
+        
+        // remove the fade effect because we need to toggle the overflow
+        // and it looks crappy when it gets cut off during the transition
+        $(this).data('editable').container.tip().removeClass('fade');
+      }).on('hidden', function(){
+        $el.parents('.imageContainer').removeClass('editing');
+      }).data('editable');
+      
+      
       return this;
     },
     events: {
       'click .permission': 'permission'
+      
     },
     permission: function(ev) {
       ev.preventDefault();
       var el = $(ev.currentTarget), id = el.attr('data-id'), model = this.model/*arguments[0].view.Photos.get(id)*/, view = this;
-      model.set('permission', model.get('permission') == 0 ? 1 : 0, {silent:true});
+      model.set('permission', model.get('permission') == 0 ? 1 : 0, {silent:false});
       model.save();
     },
     modelChanged: function() {
--- src/html/assets/themes/fabrizio1.0/templates/photos.php
+++ src/html/assets/themes/fabrizio1.0/templates/photos.php
@@ -4,7 +4,7 @@
 </div>
 <script type="tmpl/underscore" id="photo-meta">
   <div class="photo-meta">
-    <h4 class="title"><a href="/p/<%= id %>"><%= title || filenameOriginal %><a/></h4>
+    <h4 class="title"><a href="/p/<%= id %>"><%= title || filenameOriginal %></a></h4>
     <ul class="info">
       <li><a href="#"><i class="tb-icon-small-comment tb-icon-dark"></i> <span class="number">24</span></a>
       <li><a href="#"><i class="tb-icon-small-heart tb-icon-dark"></i> <span class="number">24</span></a>
