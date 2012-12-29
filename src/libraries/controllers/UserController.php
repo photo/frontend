ff18edc1e914c954c1ba79cdf5a7bc2681dd85d0
Jaisen Mathai <jaisen@jmathai.com>
2012-12-29 00:02:15 -0500

    Removing /user/settings page that is now deprected. #638

--- src/libraries/controllers/UserController.php
+++ src/libraries/controllers/UserController.php
@@ -59,38 +59,4 @@ class UserController extends BaseController
     $res = $this->api->invoke('/user/logout.json', EpiRoute::httpGet);
     $this->route->redirect('/');
   }
-
-  /**
-    * Generate a mobiel passphrase.
-    *
-    * @return void
-    */
-  public function mobilePassphrase()
-  {
-    getAuthentication()->requireAuthentication();
-    $userObj = new User;
-    $userObj->setMobilePassphrase();
-    $this->route->redirect('/user/settings');
-  }
-
-  /**
-    * User's settings page
-    *
-    * @return void
-    */
-  public function settings()
-  {
-    getAuthentication()->requireAuthentication();
-    $userObj = new User;
-    $credentials = $this->api->invoke('/oauth/list.json', EpiRoute::httpGet);
-    $groups = $this->api->invoke('/groups/list.json', EpiRoute::httpGet);
-    $webhooks = $this->api->invoke('/webhooks/list.json', EpiRoute::httpGet);
-    $plugins = $this->api->invoke('/plugins/list.json', EpiRoute::httpGet);
-    $mobilePassphrase = $userObj->getMobilePassphrase();
-    if(!empty($mobilePassphrase))
-      $mobilePassphrase['minutes'] = ceil(($mobilePassphrase['expiresAt']-time())/60);
-    $template = sprintf('%s/settings.php', $this->config->paths->templates);
-    $body = $this->template->get($template, array('crumb' => getSession()->get('crumb'), 'plugins' => $plugins['result'], 'credentials' => $credentials['result'], 'webhooks' => $webhooks['result'], 'groups' => $groups['result'], 'mobilePassphrase' => $mobilePassphrase));
-    $this->theme->display('template.php', array('body' => $body, 'page' => 'settings'));
-  }
 }
