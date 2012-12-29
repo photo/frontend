c8fd53909bc00b494e323e8bdd70b62f0e279d14
Mark Fabrizio <fabrizim@owlwatch.com>
2012-12-29 15:06:31 -0500

    Add "EditableView" private class implement editableview for inline edit views

--- src/html/assets/themes/fabrizio1.0/javascripts/data.js
+++ src/html/assets/themes/fabrizio1.0/javascripts/data.js
@@ -11,6 +11,148 @@ if( !window.op.data ) window.op.data = {};
 
 (function(exports){
   
+  /**
+   * Override the setPosition function to correct the x coordinate
+   * if it is tip extends beyond the width of the window on either side
+   */
+  $.fn.editableContainer.Constructor.prototype.setPosition = function(){
+    (function() {    
+        var $tip = this.tip()
+        , inside
+        , pos
+        , actualWidth
+        , actualHeight
+        , placement
+        , tp;
+
+        placement = typeof this.options.placement === 'function' ?
+        this.options.placement.call(this, $tip[0], this.$element[0]) :
+        this.options.placement;
+
+        inside = /in/.test(placement);
+       
+        $tip
+      //  .detach()
+      //vitalets: remove any placement class because otherwise they dont influence on re-positioning of visible popover
+        .removeClass('top right bottom left')
+        .css({ top: 0, left: 0, display: 'block' });
+      //  .insertAfter(this.$element);
+       
+        pos = this.getPosition(inside);
+
+        actualWidth = $tip[0].offsetWidth;
+        actualHeight = $tip[0].offsetHeight;
+
+        switch (inside ? placement.split(' ')[1] : placement) {
+            case 'bottom':
+                tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
+                break;
+            case 'top':
+                tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
+                break;
+            case 'left':
+                tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
+                break;
+            case 'right':
+                tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
+                break;
+        }
+        
+        // check that the x position is withinin the screen
+        if( tp.left < 0 ) {
+          var offset = tp.left-15;
+          tp.left = 5;
+          $tip.find('.arrow').css({'margin-left': offset });
+        }
+        else if( tp.left + actualWidth > $(window).width() ){
+          var offset = (tp.left+actualWidth) - ($(window).scrollLeft() + $(window).width());
+          tp.left -= (offset + 5);
+          $tip.find('.arrow').css({'margin-left': offset-5 });
+        }
+        else {
+          $tip.find('.arrow').css({'margin-left': -10 });
+        }
+        
+        // check to make sure that the y position is not off the top of the screen
+        if( tp.top < $(window).scrollTop() ){
+          // change the placement to the bottom
+          tp.top = pos.top + pos.height;
+          placement = 'bottom';
+        }
+        
+
+        $tip
+        .offset(tp)
+        .addClass(placement)
+        .addClass('in');
+        
+    }).call(this.container());
+  };
+  
+  var EditableView = Backbone.View.extend({
+    
+    getViewData : function(){
+      return this.model.toJSON();
+    },
+    
+    render : function(){
+      
+      var self = this;
+      
+      // whenever this is re-rendered, check for any old editables
+      // and hide them in case the tip is open.
+      if( this.editable ) for(var i in this.editable){
+        var e;
+        if( this.editable.hasOwnProperty(i) && this.el && (e = $(this.el).find(i).data('editable')) ){
+          e.hide();
+        }
+      }
+      
+      $(this.el).html(this.template(this.getViewData()));
+      
+      if( !this.editable ) return this;
+      
+      for( var i in this.editable ){
+        if( this.editable.hasOwnProperty(i) ){
+          
+          var $el = $(this.el).find(i);
+          
+          var config = _.extend({
+            placement: 'top',
+            url : function(params){
+              var d = new $.Deferred;
+              self.model.set(params.name, params.value, {silent:true});
+              self.model.save(null, {
+                success : function(){
+                  d.done();
+                },
+                error : function(){
+                  /**
+                   * TODO: should report the error, but I'm not sure what the
+                   * arguments / return values are.
+                   */
+                  d.reject();
+                }
+              });
+              return d;
+            }
+          }, this.editable[i]);
+          
+          // grab the "on" property
+          var on = config.on;
+          
+          delete config.on;
+          $el.editable(config);
+          $el.data('editable').view = self;
+          
+          if( on ) $el.on( on );
+        }
+      }
+      return this;
+    }
+  });
+  
+  
   /* ------------------------------- Profile ------------------------------- */
   var Profile = Backbone.Model.extend({
     sync: function(method, model) {
@@ -47,7 +189,7 @@ if( !window.op.data ) window.op.data = {};
       return response.result;
     }
   });
-  var ProfileNameView = Backbone.View.extend({
+  var ProfileNameView = EditableView.extend({
     initialize: function() {
       this.model.on('change', this.modelChanged, this);
     },
@@ -55,24 +197,21 @@ if( !window.op.data ) window.op.data = {};
     tagName: 'h4',
     className: 'profile-name-meta',
     template    :_.template($('#profile-name-meta').html()),
-    render      :function(){
-      $(this.el).html(this.template(this.model.toJSON()));
-      return this;
+    editable    : {
+      '.name.edit' : {
+        name: 'name',
+        placement: 'bottom',
+        title: 'Edit Gallery Name',
+        validate : function(value){
+          if($.trim(value) == ''){
+            return 'Please enter a name';
+          }
+          return null;
+        }
+      }
     },
     modelChanged: function() {
       this.render();
-    },
-    events: {
-      'click .name.edit': 'prompt'
-    },
-    prompt: function() {
-      var model = this.model, currentName = this.model.get('name');
-      var newName = prompt("Change your name", currentName);
-      if(newName === null || newName === currentName)
-        return;
-
-      model.set('name', newName, {silent:true});
-      model.save();
     }
   });
   var ProfileCollection = Backbone.Collection.extend({
@@ -146,135 +285,34 @@ if( !window.op.data ) window.op.data = {};
     localStorage  :'op-photos'
   });
   
-  /**
-   * Override the setPosition function to correct the x coordinate
-   * if it is tip extends beyond the width of the window on either side
-   */
-  $.fn.editableContainer.Constructor.prototype.setPosition = function(){
-    (function() {    
-        var $tip = this.tip()
-        , inside
-        , pos
-        , actualWidth
-        , actualHeight
-        , placement
-        , tp;
-
-        placement = typeof this.options.placement === 'function' ?
-        this.options.placement.call(this, $tip[0], this.$element[0]) :
-        this.options.placement;
-
-        inside = /in/.test(placement);
-       
-        $tip
-      //  .detach()
-      //vitalets: remove any placement class because otherwise they dont influence on re-positioning of visible popover
-        .removeClass('top right bottom left')
-        .css({ top: 0, left: 0, display: 'block' });
-      //  .insertAfter(this.$element);
-       
-        pos = this.getPosition(inside);
-
-        actualWidth = $tip[0].offsetWidth;
-        actualHeight = $tip[0].offsetHeight;
-
-        switch (inside ? placement.split(' ')[1] : placement) {
-            case 'bottom':
-                tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
-                break;
-            case 'top':
-                tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
-                break;
-            case 'left':
-                tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
-                break;
-            case 'right':
-                tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
-                break;
-        }
-        
-        // check that the x position is withinin the screen
-        if( tp.left < 0 ) {
-          var offset = tp.left-15;
-          tp.left = 5;
-          $tip.find('.arrow').css({'margin-left': offset });
-        }
-        else if( tp.left + actualWidth > $(window).width() ){
-          var offset = (tp.left+actualWidth) - ($(window).scrollLeft() + $(window).width());
-          tp.left -= (offset + 5);
-          $tip.find('.arrow').css({'margin-left': offset-5 });
-        }
-        else {
-          $tip.find('.arrow').css({'margin-left': -10 });
-        }
-        
-
-        $tip
-        .offset(tp)
-        .addClass(placement)
-        .addClass('in');
-        
-    }).call(this.container());
-  };
   
-  
-  var PhotoGalleryView = Backbone.View.extend({
+  var PhotoGalleryView = EditableView.extend({
     initialize: function() {
       this.model.on('change', this.modelChanged, this);
     },
     model: this.model,
     className: 'photo-meta',
     template    :_.template($('#photo-meta').html()),
-    render      :function(){
-      var self = this
-        , $el = $(this.el);
-      
-      // We need to capture the lastEditable because
-      // after a successful save, render is called to
-      // update the view. the $tip will be lost and the
-      // "hidden" event will not fire.
-      if( this.lastEditable ) {
-        this.lastEditable.hide();
-      }
-        
-      $el.html(this.template(this.model.toJSON()));
-      
-      // initialize the inline editor..
-      this.lastEditable = $el.find('.title a ').editable({
+    editable    : {
+      '.title a' : {
         name: 'title',
+        title: 'Edit Photo Title',
         placement: 'top',
-        title: 'Edit Title',
-        url: function(params){
-          
-          var d = new $.Deferred;
-          self.model.set(params.name, params.value, {silent:true});
-          self.model.save(null, {
-            success : function(){
-              d.done();
-            },
-            error : function(){
-              /**
-               * TODO: should report the error, but I'm not sure what the
-               * arguments / return values are.
-               */
-              d.reject();
-            }
-          });
-          return d;
+        on: {
+          shown: function(){
+            // var view = $(this).data('editable').view;
+            $(this).parents('.imageContainer').addClass('editing');
+            $(this).data('editable').container.setPosition();
+            
+            // remove the fade effect because we need to toggle the overflow
+            // and it looks crappy when it gets cut off during the transition
+            $(this).data('editable').container.tip().removeClass('fade');
+          },
+          hidden : function(){
+            $(this).parents('.imageContainer').removeClass('editing');
+          }
         }
-      }).on('shown', function(){
-        $el.parents('.imageContainer').addClass('editing');
-        $(this).data('editable').container.setPosition();
-        
-        // remove the fade effect because we need to toggle the overflow
-        // and it looks crappy when it gets cut off during the transition
-        $(this).data('editable').container.tip().removeClass('fade');
-      }).on('hidden', function(){
-        $el.parents('.imageContainer').removeClass('editing');
-      }).data('editable');
-      
-      
-      return this;
+      }
     },
     events: {
       'click .permission.edit': 'permission',
