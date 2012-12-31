fb59374e97a4b6832497f111ced9150746231f8f
Jaisen Mathai <jaisen@jmathai.com>
2012-12-31 14:45:21 -0500

    Adding templates for the front page. #1005

--- src/html/assets/themes/fabrizio1.0/templates/front.php
+++ src/html/assets/themes/fabrizio1.0/templates/front.php
@@ -1,1 +1,9 @@
 <?php $this->theme->display('partials/user-badge.php'); ?>
+<?php if(!empty($activities)) { ?>
+  <ol>
+    <?php foreach($activities as $activity) { ?>
+        <?php $this->theme->display(sprintf('partials/feed-%s.php', $activity[0]['type']), array('activity' => $activity)); ?>
+      <?php $i++; ?>
+    <?php } ?>
+  </ol>
+<?php } ?>
