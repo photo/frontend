7d767d7b83b8ae9c60d11491b0f57e1e25d98c9a
Mark Fabrizio <fabrizim@owlwatch.com>
2012-12-30 22:47:37 -0500

    Split data.js into namespaced files

---
+++ src/html/assets/themes/fabrizio1.0/javascripts/op/data/store/Photos.js
@@ -0,0 +1,8 @@
+/**
+ * Global Store
+ * - these may be better within an "app.js" file or something,
+ * but will leave them here for now.
+ */
+op.ns('data.store').Photos = new op.data.collection.Photo({
+  localStorage  :'op-photo'
+});
