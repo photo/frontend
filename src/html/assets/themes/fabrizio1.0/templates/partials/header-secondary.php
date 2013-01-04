dcb4b16b9a58813b64c670a4eb9ab23f0072c7de
Jaisen Mathai <jaisen@jmathai.com>
2013-01-04 11:27:34 -0500

    Adding secondary links to the upload page. #1005

--- src/html/assets/themes/fabrizio1.0/templates/partials/header-secondary.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/header-secondary.php
@@ -1,3 +1,6 @@
           <?php if($this->utility->isActiveTab('photo')) { ?>
             <li class="separator-left dropdown batch-meta"></li>
+          <?php } elseif($this->utility->isActiveTab('upload')) {?>
+            <li class="separator-left"><a href="#"><i class="tb-icon-light tb-icon-storage"></i> iPhone App</a></li>
+            <li><a href=#"><i class="tb-icon-light tb-icon-storage"></i> Android App</a></li>
           <?php } ?>
