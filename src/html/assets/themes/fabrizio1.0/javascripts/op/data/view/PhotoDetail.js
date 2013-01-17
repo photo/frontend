4461873aeba3a1776a8be007ea139f74f48850d8
Jaisen Mathai <jaisen@jmathai.com>
2013-01-17 03:16:57 -0500

    Adding functional share and download links to the photo detail page. #1005

--- src/html/assets/themes/fabrizio1.0/javascripts/op/data/view/PhotoDetail.js
+++ src/html/assets/themes/fabrizio1.0/javascripts/op/data/view/PhotoDetail.js
@@ -83,13 +83,19 @@
       return this;
     },
     events : {
-      'click .permission.edit': 'permission'
+      'click .permission.edit': 'permission',
+      'click .share': 'share'
     },
     permission: function(ev) {
       ev.preventDefault();
       var el = $(ev.currentTarget), id = el.attr('data-id'), model = this.model;
       model.set('permission', model.get('permission') == 0 ? 1 : 0, {silent:false});
       model.save();
+    },
+    share: function(ev) {
+      ev.preventDefault();
+      var $el = $(ev.currentTarget), id = $el.attr('data-id');
+      OP.Util.makeRequest('/photos/'+id+'/share.json', {}, TBX.callbacks.share, 'json', 'get');
     }
   });
   
