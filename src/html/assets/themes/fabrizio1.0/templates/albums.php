92ecff07a8226c6c813bbfad18ce08977cfa80c3
Jaisen Mathai <jaisen@jmathai.com>
2013-01-18 12:00:57 -0500

    Adding messages for if no photos or albums do not exist yet. #1005

--- src/html/assets/themes/fabrizio1.0/templates/albums.php
+++ src/html/assets/themes/fabrizio1.0/templates/albums.php
@@ -7,15 +7,22 @@
     <ul class="albums"></ul>
     <script> var initData = <?php echo json_encode($albums); ?>;</script>
   <?php } else { ?>
-    <h4>You haven't created any albums, yet.</h4>  
-    <p>
-      It's easy to get started with albums.
-      <ol>
-        <li>Click the link above to create a new album.</li>
-        <li>Type a name and create the album.</li>
-        <li>Head over to your gallery and select the photos you'd like to add.</li>
-        <li>Add them to your newly created album.</li>
-      </ol>
-    </p>
+    <?php if($this->user->isAdmin()) { ?>
+      <h4>You haven't created any albums, yet.</h4>  
+      <p>
+        It's easy to get started with albums.
+        <ol>
+          <li>Click the link above to create a new album.</li>
+          <li>Type a name and create the album.</li>
+          <li>Head over to your gallery and select the photos you'd like to add.</li>
+          <li>Add them to your newly created album.</li>
+        </ol>
+      </p>
+    <?php } else { ?>
+      <h4>This user hasn't created any albums, yet.</h4>  
+      <p>
+        You should give them a nudge to get started!
+      </p>
+    <?php } ?>
   <?php } ?>
 </div>
