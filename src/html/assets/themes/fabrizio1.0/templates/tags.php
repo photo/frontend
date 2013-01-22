39265653b12e66f6855897379b520564d84c5901
Jaisen Mathai <jaisen@jmathai.com>
2013-01-22 01:55:28 -0500

    Adding tags page. #1005

---
+++ src/html/assets/themes/fabrizio1.0/templates/tags.php
@@ -0,0 +1,19 @@
+<?php $this->theme->display('partials/user-badge.php'); ?>
+
+<div class="row">
+  <div class="span12 tags-content">
+    <div class="tags-hr"></div>
+    <?php if(!empty($tags)) { ?>
+      <ul class="tags">
+        <?php foreach($tags as $tag) { ?>
+          <li><a href="/photos/tags-<?php $this->utility->safe($tag['id']); ?>/list"><?php $this->utility->safe($tag['id']); ?></a></li>
+        <?php } ?>
+      </ul>
+    <?php } else { ?>
+        <h4>This user hasn't created any tags, yet.</h4>  
+        <p>
+          You should give them a nudge to get started!
+        </p>
+    <?php } ?>
+  </div>
+</div>
