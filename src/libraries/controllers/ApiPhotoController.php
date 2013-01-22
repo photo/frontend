a1f74d0114de4c29f5f218fd5ea5b7f355c15d6a
Matthieu Prat <matthieuprat@gmail.com>
2013-01-23 00:35:44 +0100

    Fix tag counts update upon photo deletion
    The API call in method ApiPhotoController::delete is issued before the
    photo is deleted otherwise it returns an empty result.
    
    Conflicts:
    	src/libraries/controllers/ApiPhotoController.php

--- src/libraries/controllers/ApiPhotoController.php
+++ src/libraries/controllers/ApiPhotoController.php
@@ -35,8 +35,6 @@ class ApiPhotoController extends ApiBaseController
     {
       $activityObj = new Activity;
       $activityObj->deleteForElement($id, array('photo-upload','photo-update','action-create'));
-
-      $res = $this->api->invoke("/{$this->apiVersion}/photo/{$id}/view.json");
       return $this->noContent('Photo deleted successfully', true);
     }
     else
