ec75988b35e9375431dd509de53c21d3b6cbbc37
Jaisen Mathai <jaisen@jmathai.com>
2013-01-21 23:56:05 -0500

    Adding front.php with activity. #1005

--- src/html/assets/themes/fabrizio1.0/templates/partials/feed-photo-upload.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/feed-photo-upload.php
@@ -1,8 +1,22 @@
-<li>
-  <img src="<?php $this->utility->safe($this->user->getAvatarFromEmail(50, $activity[0]['owner'])); ?>" alt="<?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?>" class="thumbnail" />
-  <?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?> uploaded <?php printf('%d %s', count($activity), $this->utility->plural(count($activity), 'photo', false)); ?>, 
-  <em><?php $this->utility->timeAsText($activity[0]['data']['dateUploaded']); ?></em>
-  <?php foreach($activity as $activityDetails) { ?>
-    <a href="<?php echo $activity[0]['data']['url']; ?>"><img src="<?php $this->utility->safe($activityDetails['data']['path100x100xCR']); ?>" class="activityfeed-thumbnail"></a>
-  <?php } ?>
+<li class="type-<?php $this->utility->safe($activity[0]['type']); ?>">
+  <div class="activity-meta">
+    <span class="activity-time"><?php $this->utility->timeAsText($activity[0]['data']['dateUploaded']); ?></span>
+    <i class="activity-type icon-upload"></i>
+  </div>
+  <img class="to" src="<?php $this->utility->safe($this->user->getAvatarFromEmail(50, $activity[0]['owner'])); ?>" alt="<?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?>" />
+  <!--<img class="from" src="http://openphotofabrizio.s3.amazonaws.com/custom/201212/7d0cae-IMG_4413_100x100xCR.jpg" />-->
+  <div class="activity-wrap">
+    <div class="activity-content">
+      <strong><?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?></strong> uploaded <?php printf('%d %s', count($activity), $this->utility->plural(count($activity), 'photo', false)); ?>
+      <ul class="unstyled">
+        <?php foreach($activity as $activityDetails) { ?>
+          <li>
+            <a href="<?php echo $activityDetails['data']['url']; ?>">
+              <img src="<?php $this->utility->safe($activityDetails['data']['path100x100xCR']); ?>" class="activityfeed-thumbnail">
+            </a>
+          </li>
+        <?php } ?>
+      </ul>
+    </div>
+  </div>
 </li>
