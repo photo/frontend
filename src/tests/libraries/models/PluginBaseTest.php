785480924cd92f78666871d3e1242b6194b9ce09
Jaisen Mathai <jaisen@jmathai.com>
2013-01-22 02:25:25 -0500

    Unit tests for plugins. #529 #556

--- src/tests/libraries/models/PluginBaseTest.php
+++ src/tests/libraries/models/PluginBaseTest.php
@@ -35,15 +35,51 @@ class PluginBaseTest extends PHPUnit_Framework_TestCase
     $this->assertNull($res);
   }
 
-  public function testRenderHeadSuccess()
-  {
-    $res = $this->pluginBase->RenderHead(array());
-    $this->assertNull($res);
-  }
-
   public function testOnViewSuccess()
   {
     $res = $this->pluginBase->onView(array());
+    $this->assertNull($res);
+  }
+
+  public function testOnPhotoUploadSuccess()
+  {
+    $res = $this->pluginBase->onPhotoUpload(array());
+    $this->assertNull($res);
+  }
+
+  public function testOnPhotoUploadedSuccess()
+  {
+    $res = $this->pluginBase->onPhotoUploaded(array());
+    $this->assertNull($res);
+  }
+
+  public function testRenderHeadSuccess()
+  {
+    $res = $this->pluginBase->renderHead(array());
+    $this->assertNull($res);
+  }
+
+  public function testRenderBodySuccess()
+  {
+    $res = $this->pluginBase->renderBody(array());
+    $this->assertNull($res);
+  }
+
+  public function testRenderPhotoDetailSuccess()
+  {
+    $res = $this->pluginBase->renderPhotoDetail(array());
+    $this->assertNull($res);
+  }
+
+  public function testRenderPhotoUploadedSuccess()
+  {
+    $res = $this->pluginBase->renderPhotoUploaded(array());
+    $this->assertNull($res);
+  }
+
+  public function testRenderFooterSuccess()
+  {
+    $res = $this->pluginBase->renderFooter(array());
     $this->assertNull($res);
   }
 }
