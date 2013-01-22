400724afc1a0b5a5dc9583ea5d6295995639397e
Jaisen Mathai <jaisen@jmathai.com>
2013-01-22 01:55:28 -0500

    Adding tags page. #1005

--- src/html/assets/themes/fabrizio1.0/templates/albums.php
+++ src/html/assets/themes/fabrizio1.0/templates/albums.php
@@ -1,28 +1,30 @@
 <?php $this->theme->display('partials/user-badge.php'); ?>
 
-<div class="album-grid">
-  <div class="album-grid-hr"></div>
+<div class="row">
+  <div class="span12 album-grid">
+    <div class="album-grid-hr"></div>
 
-  <?php if(!empty($albums) ) { ?>
-    <ul class="albums"></ul>
-    <script> var initData = <?php echo json_encode($albums); ?>;</script>
-  <?php } else { ?>
-    <?php if($this->user->isAdmin()) { ?>
-      <h4>You haven't created any albums, yet.</h4>  
-      <p>
-        It's easy to get started with albums.
-        <ol>
-          <li>Click the link above to create a new album.</li>
-          <li>Type a name and create the album.</li>
-          <li>Head over to your gallery and select the photos you'd like to add.</li>
-          <li>Add them to your newly created album.</li>
-        </ol>
-      </p>
+    <?php if(!empty($albums) ) { ?>
+      <ul class="albums"></ul>
+      <script> var initData = <?php echo json_encode($albums); ?>;</script>
     <?php } else { ?>
-      <h4>This user hasn't created any albums, yet.</h4>  
-      <p>
-        You should give them a nudge to get started!
-      </p>
+      <?php if($this->user->isAdmin()) { ?>
+        <h4>You haven't created any albums, yet.</h4>  
+        <p>
+          It's easy to get started with albums.
+          <ol>
+            <li>Click the link above to create a new album.</li>
+            <li>Type a name and create the album.</li>
+            <li>Head over to your gallery and select the photos you'd like to add.</li>
+            <li>Add them to your newly created album.</li>
+          </ol>
+        </p>
+      <?php } else { ?>
+        <h4>This user hasn't created any albums, yet.</h4>  
+        <p>
+          You should give them a nudge to get started!
+        </p>
+      <?php } ?>
     <?php } ?>
-  <?php } ?>
+  </div>
 </div>
