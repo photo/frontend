14703a0e1afc99a7667a10b63d85cf79861da391
Mark Fabrizio <fabrizim@owlwatch.com>
2013-01-26 18:40:01 -0500

    Add tag files as necessary

---
+++ src/html/assets/themes/fabrizio1.0/javascripts/op/data/model/Tag.js
@@ -0,0 +1,7 @@
+(function($){
+  op.ns('data.model').Tag = Backbone.Model.extend({
+    /**
+     * TODO - implement the sync method for Tags
+     */
+  });
+})(jQuery);
---
+++ src/html/assets/themes/fabrizio1.0/javascripts/op/data/store/Tags.js
@@ -0,0 +1,6 @@
+/**
+ * Global Store
+ * - these may be better within an "app.js" file or something,
+ * but will leave them here for now.
+ */
+op.ns('data.store').Tags = new op.data.collection.Tag();
