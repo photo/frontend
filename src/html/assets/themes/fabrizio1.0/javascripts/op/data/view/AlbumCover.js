af85839f749d50b9b5a47130ea43a02c16314f8e
Jaisen Mathai <jaisen@jmathai.com>
2013-01-29 23:59:47 -0500

    Adding ability to delete an album. Closes #1117

--- src/html/assets/themes/fabrizio1.0/javascripts/op/data/view/AlbumCover.js
+++ src/html/assets/themes/fabrizio1.0/javascripts/op/data/view/AlbumCover.js
@@ -19,8 +19,24 @@
         }
       }
     },
+    events: {
+      'click .delete': 'delete'
+    },
     modelChanged: function() {
       this.render();
+    },
+    modelDestroyed: function() {
+      var $el = $('.album-'+this.get('id'));
+      $el.fadeTo('medium', .25);
+    },
+    delete: function(ev) {
+      ev.preventDefault();
+      var $el = $(ev.target), id = $el.attr('data-id'), model = this.model, ask;
+      ask = prompt('Type DELETE if you\'d like to delete the album '+model.get('name') + '.');
+      if(ask === 'DELETE')
+        model.destroy({success: this.modelDestroyed.bind(model), error: TBX.notification.display.generic.error});
+      else
+        TBX.notification.show('Your request to delete ' + model.get('name') + ' was cancelled.', 'flash', 'error');
     }
   });
 })(jQuery);
