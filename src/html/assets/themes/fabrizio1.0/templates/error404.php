91927ec602c4d1b9f4fb9abb1f9dcd564aa81aa7
Nico Kaiser <nico@kaiser.me>
2013-02-07 07:10:19 +0100

    Typo veiw -> view

--- src/html/assets/themes/fabrizio1.0/templates/error404.php
+++ src/html/assets/themes/fabrizio1.0/templates/error404.php
@@ -7,7 +7,7 @@
       The exact cause for this error varies but some common reasons include:
       <ul>
         <li>You may have mistyped the URL or clicked a bad link.</li>
-        <li>You're trying to access a private photo you don't have access to veiw.</li>
+        <li>You're trying to access a private photo you don't have access to view.</li>
         <li>Another reason we haven't thought of yet.</li>
       </ul>
     </p>
