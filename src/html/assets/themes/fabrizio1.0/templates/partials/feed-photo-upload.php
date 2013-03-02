3b78f5e1337f56619a788dc5fab19ee14d3f4468
Jaisen Mathai <jaisen@jmathai.com>
2013-03-02 15:35:35 -0800

    Removing gravatar from profile icon. Closes #1162

--- src/html/assets/themes/fabrizio1.0/templates/partials/feed-photo-upload.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/feed-photo-upload.php
@@ -3,8 +3,11 @@
     <span class="activity-time"><?php $this->utility->timeAsText($activity[0]['data']['dateUploaded']); ?></span>
     <i class="activity-type icon-upload"></i>
   </div>
-  <img class="to" src="<?php $this->utility->safe($this->user->getAvatarFromEmail(50, $activity[0]['owner'])); ?>" alt="<?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?>" />
-  <!--<img class="from" src="http://openphotofabrizio.s3.amazonaws.com/custom/201212/7d0cae-IMG_4413_100x100xCR.jpg" />-->
+  <?php if(stristr($avatar = $this->user->getAvatarFromEmail(50, $activity[0]['owner']), 'gravatar.com') === false) { ?>
+    <img class="to" src="<?php $this->utility->safe($avatar); ?>" alt="<?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?>">
+  <?php } else { ?>
+    <i class="to icon-user" alt="<?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?>"></i>
+  <?php } ?>
   <div class="activity-wrap">
     <div class="activity-content">
       <strong><?php $this->utility->safe($this->user->getNameFromEmail($activity[0]['owner'])); ?></strong> uploaded <?php printf('%d %s', count($activity), $this->utility->plural(count($activity), 'photo', false)); ?>
