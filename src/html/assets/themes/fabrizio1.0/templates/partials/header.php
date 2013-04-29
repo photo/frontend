b86c8909d70ca5f886b0acd7e4821d09631726e7
Jaisen Mathai <jaisen@jmathai.com>
2013-04-29 16:41:13 -0700

    Swapping signin/signup links. #638

--- src/html/assets/themes/fabrizio1.0/templates/partials/header.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/header.php
@@ -24,10 +24,10 @@
           </div>
         <?php } else { ?>
           <div class="user">
+            <a href="/user/login?r=<?php $this->utility->safe($_SERVER['REQUEST_URI']); ?>" class="btn btn-theme-secondary">Sign In</a>
             <?php if($this->config->site->displaySignupLink == 1) { ?>
               <a href="<?php $this->utility->safe($this->config->site->displaySignupUrl); ?>" class="btn btn-brand btn-arrow">Sign Up</a>
             <?php } ?>
-            <a href="/user/login?r=<?php $this->utility->safe($_SERVER['REQUEST_URI']); ?>" class="btn btn-theme-secondary">Sign In</a>
           </div>
         <?php } ?>
         
