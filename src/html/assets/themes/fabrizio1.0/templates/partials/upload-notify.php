626f3d5379394dbc7e857ec5c79d34095d493e3b
Jaisen Mathai <jaisen@jmathai.com>
2013-12-06 13:33:40 -0800

    Fixing logic in notification tempalte

--- src/html/assets/themes/fabrizio1.0/templates/partials/upload-notify.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/upload-notify.php
@@ -1,6 +1,6 @@
 Hello,
 
-We wanted to let you know that <?php if(empty($count)) { ?>by <?php $this->utility->safe($count); ?> <?php } ?>photos have been uploaded <?php if(empty($uploader)) { ?>by <?php $this->utility->safe($uploader); ?> <?php } ?> to <?php $this->utility->safe($albumName); ?>.
+We wanted to let you know that <?php if(!empty($count)) { ?>by <?php $this->utility->safe($count); ?> <?php } ?>photos have been uploaded <?php if(!empty($uploader)) { ?>by <?php $this->utility->safe($uploader); ?> <?php } ?> to <?php $this->utility->safe($albumName); ?>.
 
 You can view the photos at <?php $this->utility->safe($albumUrl); ?>.
 
