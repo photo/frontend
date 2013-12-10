de9ba8d566ce49789da1d828af26d573392c0ee1
Jaisen Mathai <jaisen@jmathai.com>
2013-12-10 00:11:06 -0800

    Adding fixed class to invite uploader text field in modal. #1416
    Conflicts:
    	src/html/assets/themes/fabrizio1.0/config/settings.ini

--- src/templates/upload-token-dialog.php
+++ src/templates/upload-token-dialog.php
@@ -1,7 +1,7 @@
 <h2>Enter your name or nickname</h2>
 <form class="form-stacked uploadTokenDialog">
   <div class="control-group">
-    <input type="text" name="identifier" placeholder="your name or nickname" class="input-large">
+    <input type="text" name="identifier" placeholder="your name or nickname" class="input-large fixed">
     <div><small><i class="icon-info-sign"></i> This helps the account owner know these are your photos</small></div>
   </div>
   <div class="btn-toolbar">
