a30710edaf3ffda895e55bb984cf54fe33960dfb
Jaisen Mathai <jaisen@jmathai.com>
2014-05-21 14:45:13 -0700

    Adding `activity` columns to database. Closes #1486

--- src/configs/upgrade/db/mysql/mysql-4.0.2.php
+++ src/configs/upgrade/db/mysql/mysql-4.0.2.php
@@ -8,6 +8,27 @@ SQL;
 
 mysql_4_0_2($sql);
 
+$sql = <<<SQL
+ ALTER TABLE `{$this->mySqlTablePrefix}album` ADD `dateLastPhotoAdded` INT NOT NULL DEFAULT '0';
+SQL;
+
+mysql_4_0_2($sql);
+$sql = <<<SQL
+ ALTER TABLE `{$this->mySqlTablePrefix}photo` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE ;
+SQL;
+
+mysql_4_0_2($sql);
+$sql = <<<SQL
+ ALTER TABLE `{$this->mySqlTablePrefix}elementAlbum` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE ;
+SQL;
+
+mysql_4_0_2($sql);
+$sql = <<<SQL
+ ALTER TABLE `{$this->mySqlTablePrefix}elementTag` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE ;
+SQL;
+
+mysql_4_0_2($sql);
+
 function mysql_4_0_2($sql, $params = array())
 {
   try
