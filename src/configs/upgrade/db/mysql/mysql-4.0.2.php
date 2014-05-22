cf3a72cd666cb428ef25c2c1628669dfde3a664c
Jaisen Mathai <jaisen@jmathai.com>
2014-05-21 21:12:12 -0700

    Adding db version update. #1486

--- src/configs/upgrade/db/mysql/mysql-4.0.2.php
+++ src/configs/upgrade/db/mysql/mysql-4.0.2.php
@@ -6,28 +6,36 @@ $sql = <<<SQL
  ALTER TABLE `{$this->mySqlTablePrefix}activity` ADD `elementId` VARCHAR( 6 ) NOT NULL AFTER `type` 
 SQL;
 
-mysql_4_0_2($sql);
+$status = $status && mysql_4_0_2($sql);
 
 $sql = <<<SQL
  ALTER TABLE `{$this->mySqlTablePrefix}album` ADD `dateLastPhotoAdded` INT NOT NULL DEFAULT '0';
 SQL;
 
-mysql_4_0_2($sql);
+$status = $status && mysql_4_0_2($sql);
+
 $sql = <<<SQL
  ALTER TABLE `{$this->mySqlTablePrefix}photo` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE ;
 SQL;
 
-mysql_4_0_2($sql);
+$status = $status && mysql_4_0_2($sql);
+
 $sql = <<<SQL
  ALTER TABLE `{$this->mySqlTablePrefix}elementAlbum` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE ;
 SQL;
 
-mysql_4_0_2($sql);
+$status = $status && mysql_4_0_2($sql);
+
 $sql = <<<SQL
  ALTER TABLE `{$this->mySqlTablePrefix}elementTag` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE ;
 SQL;
 
-mysql_4_0_2($sql);
+$status = $status && mysql_4_0_2($sql);
+
+$sql = <<<SQL
+  UPDATE `{$this->mySqlTablePrefix}admin` SET `value`=:version WHERE `key`=:key;
+SQL;
+$status = $status && mysql_4_0_2($sql, array(':key' => 'version', ':version' => '4.0.2'));
 
 function mysql_4_0_2($sql, $params = array())
 {
@@ -44,8 +52,4 @@ function mysql_4_0_2($sql, $params = array())
   return true;
 }
 
-// Gh-1279
-// We return true here since the only reason to include this alter is in case
-//  a user upgraded prior to this patch and we'll run it with the table prefix
-//  and fail silently if it doesn't take
-return true;
+return $status;
