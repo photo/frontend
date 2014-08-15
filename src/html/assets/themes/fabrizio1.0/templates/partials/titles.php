2ceabdedabd6ed9ed4f9485fdc9f3ce25a74b19d
Hubert Figuiere <hub@figuiere.net>
2014-08-15 00:41:26 -0400

    Bug #1477 - Integrate BetterTitles into the core.

--- src/html/assets/themes/fabrizio1.0/templates/partials/titles.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/titles.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * BetterPageTitles
- *
- * @author Jaisen Mathai <jaisen@jmathai.com>
- */
-class BetterPageTitlesPlugin extends PluginBase
-{
-  public function __construct()
-  {
-    parent::__construct();
-  }
-
-  public function renderHead()
-  {
-    parent::renderHead();
     $user = new User;
     $page = $this->plugin->getData('page');
     $username = $user->getNameFromEmail($this->config->user->email);
@@ -26,25 +11,24 @@ class BetterPageTitlesPlugin extends PluginBase
           $prefix = sprintf('%s - ', $photo['title']);
         elseif($photo['filenameOriginal'] != '')
           $prefix = sprintf('%s - ', $photo['filenameOriginal']);
-    return <<<MKP
+    print <<<MKP
 <title>{$prefix}{$username}'s photos - Trovebox</title>
 MKP;
         break;
       case 'photos':
-    return <<<MKP
+    print <<<MKP
 <title>{$username}'s photos - Trovebox</title>
 MKP;
         break;
       case 'tags':
-    return <<<MKP
+    print <<<MKP
 <title>{$username}'s tags - Trovebox</title>
 MKP;
         break;
       default:
-    return <<<MKP
+    print <<<MKP
 <title>{$username}'s Photo site - Trovebox</title>
 MKP;
         break;
     }
-  }
-}
+?>
