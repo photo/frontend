Xin-Tong/businesslogic

Adding proper check using isAdmin in getAlbum method. Closes #1426

src/libraries/adapters/DatabaseMySql.php
@@ -354,9 +354,13 @@ class DatabaseMySql implements DatabaseInterface
     $album = $this->db->one("SELECT * FROM `{$this->mySqlTablePrefix}album` WHERE `owner`=:owner AND `id`=:id ",
       array(':id' => $id, ':owner' => $this->owner));
 
+    // we return false if we don't get an album or
+    // if the token was not validated AND
+    //  the user isn't an admin
+    //  there are no public photos AND
     if($album === false)
       return false;
-    else if(!$validatedToken && (empty($email) || $this->owner !== $email || $this->getActor() !== $email) && $album['countPublic'] == 0)
+    else if(!$validatedToken && !$this->isAdmin() && $album['countPublic'] == 0)
       return false;
 
     $album = $this->normalizeAlbum($album);
@@ -2237,7 +2241,8 @@ class DatabaseMySql implements DatabaseInterface
       return $this->isAdmin;
 
     $user = new User;
-    return $user->isAdmin();
+    $this->isAdmin = $user->isAdmin();
+    return $this->isAdmin;
   }
 
   /**

photo/frontend

Conflicts:
	src/libraries/adapters/DatabaseMySql.php

src/libraries/adapters/DatabaseMySql.php
@@ -313,14 +313,18 @@ class DatabaseMySql implements DatabaseInterface
     * @param string $email email of viewer to determine which albums they have access to
     * @return mixed Array on success, FALSE on failure
     */
-  public function getAlbum($id, $email)
+  public function getAlbum($id, $email, $validatedPermission = false)
   {
     $album = $this->db->one("SELECT * FROM `{$this->mySqlTablePrefix}album` WHERE `owner`=:owner AND `id`=:id ",
       array(':id' => $id, ':owner' => $this->owner));
 
+    // we return false if we don't get an album or
+    // if the token was not validated AND
+    //  the user isn't an admin
+    //  there are no public photos AND
     if($album === false)
       return false;
-    else if((empty($email) || $this->owner !== $email || $this->getActor() !== $email) && $album['countPublic'] == 0)
+    else if(!$validatedPermission && !$this->isAdmin() && $album['countPublic'] == 0)
       return false;
 
     $album = $this->normalizeAlbum($album);
@@ -1947,7 +1951,8 @@ class DatabaseMySql implements DatabaseInterface
       return $this->isAdmin;
 
     $user = new User;
-    return $user->isAdmin();
+    $this->isAdmin = $user->isAdmin();
+    return $this->isAdmin;
   }
 
   /**