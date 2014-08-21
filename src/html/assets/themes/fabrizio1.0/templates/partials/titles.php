2290042983ff38c7b7c0638a6ad1d1b3930423b7
Jaisen Mathai <jaisen@jmathai.com>
2014-08-21 15:48:08 -0700

    Refactoring `partials/titles.php` to be more "template-like". Closes #1477

--- src/html/assets/themes/fabrizio1.0/templates/partials/titles.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/titles.php
@@ -1,34 +1,29 @@
 <?php
-    $user = new User;
-    $page = $this->plugin->getData('page');
-    $username = $utilityObj->safe($user->getNameFromEmail($this->config->user->email), false);
-    switch($page)
-    {
-      case 'photo-detail':
-        $photo = $this->plugin->getData('photo');
-        $prefix = '';
-        if($photo['title'] != '')
-          $prefix = sprintf('%s - ', $utilityObj->safe($photo['title'], false));
-        elseif($photo['filenameOriginal'] != '')
-          $prefix = sprintf('%s - ', $photo['filenameOriginal']);
-    print <<<MKP
-<title>{$prefix}{$username}'s photos - Trovebox</title>
-MKP;
-        break;
-      case 'photos':
-    print <<<MKP
-<title>{$username}'s photos - Trovebox</title>
-MKP;
-        break;
-      case 'tags':
-    print <<<MKP
-<title>{$username}'s tags - Trovebox</title>
-MKP;
-        break;
-      default:
-    print <<<MKP
-<title>{$username}'s Photo site - Trovebox</title>
-MKP;
-        break;
-    }
+  $user = new User;
+  $utilityObj = new Utility;
+  $page = $this->plugin->getData('page');
+  $username = $utilityObj->safe($user->getNameFromEmail($this->config->user->email), false);
 ?>
+
+<title>
+<?php if($page === 'photo-detail') { ?>
+  <?php /* done in JS */ ?>
+<?php } elseif($page === 'photos') { ?>
+  <?php $album = $this->plugin->getData('album'); ?>
+  <?php $tags = $this->plugin->getData('tags'); ?>
+  <?php if($album) { ?>
+    <?php $this->utility->safe(sprintf("%s / Album / %s", $username, $album['name'])); ?>
+  <?php } elseif($tags) { ?>
+    <?php $this->utility->safe(sprintf("%s / Tags / %s", $username, implode(', ', $tags))); ?>
+  <?php } else { ?>
+    <?php $this->utility->safe(sprintf("%s / Photos", $username)); ?>
+  <?php } ?>
+<?php } elseif($page === 'albums') { ?>
+  <?php $this->utility->safe(sprintf("%s / Album", $username)); ?>
+<?php } elseif($page === 'tags') { ?>
+  <?php $this->utility->safe(sprintf("%s / Tags", $username)); ?>
+<?php } else { ?>
+  <?php $this->utility->safe(sprintf("%s", $username)); ?>
+<?php } ?>
+ / Trovebox
+</title>
