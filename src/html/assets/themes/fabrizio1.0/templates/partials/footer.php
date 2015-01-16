bc9a73ba9557e1a3bab0ee31b26f7ec7ec8549e2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-01-16 21:02:05 +0100

    Misc updates in the footer.php and front.php template.

--- src/html/assets/themes/fabrizio1.0/templates/partials/footer.php
+++ src/html/assets/themes/fabrizio1.0/templates/partials/footer.php
@@ -5,15 +5,10 @@
             <ul class="unstyled">
               <li><a href="/">Home</a></li>
               <li><a href="https://github.com/photo" target="_blank">Source</a></li>
+              <li><a href="https://blog.cedricbonhomme.org" target="_blank">Blog</a></li>
+              <li><a href="https://wiki.cedricbonhomme.org" target="_blank">Wiki</a></li>
             </ul>
             <div class="copyright">&copy; <?php echo date('Y'); ?></div>
-          </div>
-          <div class="span8 badges">
-            <ul class="unstyled pull-right">
-              <li class="supported-by">Supported by</li>
-              <li><a href="http://shuttleworthfoundation.org/" class="shuttleworth" target="_blank"><span class="offscreen">Shuttleworth Foundation grant recipient</span></a></li>
-              <li><a href="https://webfwd.org/" class="webfwd" target="_blank"><span class="offscreen">Mozilla WebFWD fellow</span></a></li>
-            </ul>
           </div>
         </div>
       </div>
